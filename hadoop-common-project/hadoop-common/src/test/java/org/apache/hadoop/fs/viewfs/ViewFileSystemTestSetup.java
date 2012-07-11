3f0002c90a479c72e9743e82b3c7ba6178e62938
Andrew Klochkov <aklochkov@griddynamics.com>
2012-07-10 23:26:37 -0700

    Fix viewFS tests for std jenkins layout

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/viewfs/ViewFileSystemTestSetup.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/viewfs/ViewFileSystemTestSetup.java
@@ -61,8 +61,7 @@ public class ViewFileSystemTestSetup {
     // path of testdir.
     String testDir = FileSystemTestHelper.getTestRootPath(fsTarget).toUri()
         .getPath();
-    int indexOf2ndSlash = testDir.indexOf('/', 1);
-    String testDirFirstComponent = testDir.substring(0, indexOf2ndSlash);
+    String testDirFirstComponent = getDirFirstComponent(testDir);
     Log.info("add link " + testDirFirstComponent + " -- " + fsTarget.makeQualified(
         new Path(testDirFirstComponent)).toUri());
     ConfigUtil.addLink(conf, testDirFirstComponent, fsTarget.makeQualified(
@@ -71,10 +70,11 @@ public class ViewFileSystemTestSetup {
     // viewFs://home => fsTarget://home
     String homeDirRoot = fsTarget.getHomeDirectory()
         .getParent().toUri().getPath();
-    Log.info("add link " + homeDirRoot + " -- " + 
-        fsTarget.makeQualified(new Path(homeDirRoot)).toUri());
-    ConfigUtil.addLink(conf, homeDirRoot,
-        fsTarget.makeQualified(new Path(homeDirRoot)).toUri());
+    String homeDirFirstComponent = getDirFirstComponent(homeDirRoot);
+    Log.info("add link " + homeDirFirstComponent + " -- " + 
+        fsTarget.makeQualified(new Path(homeDirFirstComponent)).toUri());
+    ConfigUtil.addLink(conf, homeDirFirstComponent,
+        fsTarget.makeQualified(new Path(homeDirFirstComponent)).toUri());
     ConfigUtil.setHomeDirConf(conf, homeDirRoot);
     Log.info("Home dir base " + homeDirRoot);
 
@@ -96,4 +96,14 @@ public class ViewFileSystemTestSetup {
     conf.set("fs.viewfs.impl", ViewFileSystem.class.getName());
     return conf; 
   }
+
+  private static String getDirFirstComponent(String dir) {
+	System.out.println("Get first component of dir " + dir);
+    int indexOf2ndSlash = dir.indexOf('/', 1);
+    if (indexOf2ndSlash == -1)
+      return dir;
+    String testDirFirstComponent = dir.substring(0, indexOf2ndSlash);
+    return testDirFirstComponent;
+  }
+
 }
