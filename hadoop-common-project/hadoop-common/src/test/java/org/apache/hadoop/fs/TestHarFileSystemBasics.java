3f79dc9cf92d47909c720636e551aee55fa83796
Ivan Veselovsky <iveselovsky@griddynamics.com>
2012-10-31 23:11:06 +0400

    HADOOP-8992 -- (1) corrected class formatting by the review comments; (2) used logging instead of System.out

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/TestHarFileSystemBasics.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/TestHarFileSystemBasics.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.hadoop.fs;
 
 import static org.junit.Assert.assertEquals;
@@ -8,6 +26,8 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.permission.FsPermission;
 import org.junit.After;
@@ -16,28 +36,35 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * This test class checks basic operations with {@link HarFileSystem} including various initialization cases,
- * getters, and modification methods. 
- *  
- * NB: to run this test from an IDE make sure the 
- * folder "hadoop-common-project/hadoop-common/src/main/resources/" is added as a source path.
- * This will allow the system to pick up the "core-default.xml" and "META-INF/services/..." resources 
- * from the class-path in the runtime.
+ * This test class checks basic operations with {@link HarFileSystem} including
+ * various initialization cases, getters, and modification methods.
+ * 
+ * NB: to run this test from an IDE make sure the folder
+ * "hadoop-common-project/hadoop-common/src/main/resources/" is added as a
+ * source path. This will allow the system to pick up the "core-default.xml" and
+ * "META-INF/services/..." resources from the class-path in the runtime.
  */
 public class TestHarFileSystemBasics {
 
-  private static final String ROOT_PATH = System.getProperty("test.build.data", "build/test/data");
-  private static final Path rootPath = new Path(new File(ROOT_PATH).getAbsolutePath() + "/localfs");
-  private static final Path harPath = new Path(rootPath, "path1/path2/my.har"); // NB: .har suffix is necessary
+  private static final Log LOG = LogFactory
+      .getLog(TestHarFileSystemBasics.class);
+
+  private static final String ROOT_PATH = System.getProperty("test.build.data",
+      "build/test/data");
+  private static final Path rootPath = new Path(
+      new File(ROOT_PATH).getAbsolutePath() + "/localfs");
+  // NB: .har suffix is necessary
+  private static final Path harPath = new Path(rootPath, "path1/path2/my.har");
 
   private FileSystem localFileSystem;
   private HarFileSystem harFileSystem;
   private Configuration conf;
-  
+
   /*
-   * creates and returns fully initialized HarFileSystem  
+   * creates and returns fully initialized HarFileSystem
    */
-  private HarFileSystem createHarFileSysten(final Configuration conf) throws Exception {
+  private HarFileSystem createHarFileSysten(final Configuration conf)
+      throws Exception {
     localFileSystem = FileSystem.getLocal(conf);
     localFileSystem.initialize(new URI("file:///"), conf);
     localFileSystem.mkdirs(rootPath);
@@ -45,18 +72,18 @@ public class TestHarFileSystemBasics {
     final Path indexPath = new Path(harPath, "_index");
     final Path masterIndexPath = new Path(harPath, "_masterindex");
     localFileSystem.createNewFile(indexPath);
-      assertTrue(localFileSystem.exists(indexPath));
+    assertTrue(localFileSystem.exists(indexPath));
     localFileSystem.createNewFile(masterIndexPath);
-      assertTrue(localFileSystem.exists(masterIndexPath));
-    
+    assertTrue(localFileSystem.exists(masterIndexPath));
+
     writeVersionToMasterIndexImpl(HarFileSystem.VERSION);
-      
+
     final HarFileSystem harFileSystem = new HarFileSystem(localFileSystem);
-    final URI uri = new URI("har://" + harPath.toString()); 
+    final URI uri = new URI("har://" + harPath.toString());
     harFileSystem.initialize(uri, conf);
     return harFileSystem;
   }
-  
+
   private void writeVersionToMasterIndexImpl(int version) throws IOException {
     final Path masterIndexPath = new Path(harPath, "_masterindex");
     // write Har version into the master index:
@@ -69,19 +96,20 @@ public class TestHarFileSystemBasics {
       fsdos.close();
     }
   }
-  
+
   @Before
   public void before() throws Exception {
     final File rootDirIoFile = new File(rootPath.toUri().getPath());
     rootDirIoFile.mkdirs();
     if (!rootDirIoFile.exists()) {
-      throw new IOException("Failed to create temp directory ["+rootDirIoFile.getAbsolutePath()+"]");
+      throw new IOException("Failed to create temp directory ["
+          + rootDirIoFile.getAbsolutePath() + "]");
     }
     // create Har to test:
     conf = new Configuration();
     harFileSystem = createHarFileSysten(conf);
   }
-  
+
   @After
   public void after() throws Exception {
     // close Har FS:
@@ -90,18 +118,19 @@ public class TestHarFileSystemBasics {
       harFS.close();
       harFileSystem = null;
     }
-    // cleanup: delete all the temporary files: 
+    // cleanup: delete all the temporary files:
     final File rootDirIoFile = new File(rootPath.toUri().getPath());
     if (rootDirIoFile.exists()) {
       FileUtil.fullyDelete(rootDirIoFile);
     }
     if (rootDirIoFile.exists()) {
-      throw new IOException("Failed to delete temp directory ["+rootDirIoFile.getAbsolutePath()+"]");
+      throw new IOException("Failed to delete temp directory ["
+          + rootDirIoFile.getAbsolutePath() + "]");
     }
   }
-  
+
   // ======== Positive tests:
-  
+
   @Test
   public void testPositiveHarFileSystemBasics() throws Exception {
     // check Har version:
@@ -111,7 +140,7 @@ public class TestHarFileSystemBasics {
     final URI harUri = harFileSystem.getUri();
     assertEquals(harPath.toUri().getPath(), harUri.getPath());
     assertEquals("har", harUri.getScheme());
-    
+
     // check Har home path:
     final Path homePath = harFileSystem.getHomeDirectory();
     assertEquals(harPath.toUri().getPath(), homePath.toUri().getPath());
@@ -119,31 +148,32 @@ public class TestHarFileSystemBasics {
     // check working directory:
     final Path workDirPath0 = harFileSystem.getWorkingDirectory();
     assertEquals(homePath, workDirPath0);
-    
-    // check that its impossible to reset the working directory (#setWorkingDirectory should have no effect): 
+
+    // check that its impossible to reset the working directory
+    // (#setWorkingDirectory should have no effect):
     harFileSystem.setWorkingDirectory(new Path("/foo/bar"));
     assertEquals(workDirPath0, harFileSystem.getWorkingDirectory());
   }
-  
+
   @Test
   public void testPositiveNewHarFsOnTheSameUnderlyingFs() throws Exception {
     // Init 2nd har file system on the same underlying FS, so the
     // metadata gets reused:
     final HarFileSystem hfs = new HarFileSystem(localFileSystem);
-    final URI uri = new URI("har://" + harPath.toString()); 
+    final URI uri = new URI("har://" + harPath.toString());
     hfs.initialize(uri, new Configuration());
     // the metadata should be reused from cache:
     assertTrue(hfs.getMetadata() == harFileSystem.getMetadata());
-  }  
-  
+  }
+
   @Test
   public void testPositiveInitWithoutUnderlyingFS() throws Exception {
     // Init HarFS with no constructor arg, so that the underlying FS object
     // is created on demand or got from cache in #initialize() method.
     final HarFileSystem hfs = new HarFileSystem();
-    final URI uri = new URI("har://" + harPath.toString()); 
+    final URI uri = new URI("har://" + harPath.toString());
     hfs.initialize(uri, new Configuration());
-  }  
+  }
 
   // ========== Negative:
 
@@ -161,8 +191,8 @@ public class TestHarFileSystemBasics {
     } catch (IOException ioe) {
       // ok
     }
-  }  
-  
+  }
+
   @Test
   public void testNegativeGetHarVersionOnNotInitializedFS() throws Exception {
     final HarFileSystem hfs = new HarFileSystem(localFileSystem);
@@ -170,100 +200,103 @@ public class TestHarFileSystemBasics {
       int version = hfs.getHarVersion();
       Assert.fail("Exception expected, but got a Har version " + version + ".");
     } catch (IOException ioe) {
-      System.out.println("ok: "+ioe);
+      LOG.info("ok: " + ioe);
     }
-  }  
-  
+  }
+
   @Test
   public void testNegativeInitWithAnUnsupportedVersion() throws Exception {
-    // NB: should wait at least 1 second to ensure the timestamp of the master index will change
-    // upon the writing, because Linux seems to update the file modification time with 1 second accuracy: 
+    // NB: should wait at least 1 second to ensure the timestamp of the master
+    // index will change upon the writing, because Linux seems to update the
+    // file modification
+    // time with 1 second accuracy:
     Thread.sleep(1000);
     // write an unsupported version:
     writeVersionToMasterIndexImpl(7777);
     // init the Har:
     final HarFileSystem hfs = new HarFileSystem(localFileSystem);
-    
+
     // the metadata should *not* be reused from cache:
     assertFalse(hfs.getMetadata() == harFileSystem.getMetadata());
-    
+
     final URI uri = new URI("har://" + harPath.toString());
     try {
       hfs.initialize(uri, new Configuration());
       Assert.fail("IOException expected.");
     } catch (IOException ioe) {
-      System.out.println("ok: "+ioe);
+      LOG.info("ok: " + ioe);
     }
   }
-  
+
   @Test
   public void testNegativeHarFsModifications() throws Exception {
     // all the modification methods of HarFS must lead to IOE.
     final Path fooPath = new Path(rootPath, "foo/bar");
     localFileSystem.createNewFile(fooPath);
     try {
-      harFileSystem.create(fooPath, new FsPermission("+rwx"), true, 1024, (short)88, 1024, null);
+      harFileSystem.create(fooPath, new FsPermission("+rwx"), true, 1024,
+          (short) 88, 1024, null);
       Assert.fail("IOException expected.");
     } catch (IOException ioe) {
-      System.out.println("ok: "+ioe);
+      LOG.info("ok: " + ioe);
     }
-    
+
     try {
-      harFileSystem.setReplication(fooPath, (short)55);
+      harFileSystem.setReplication(fooPath, (short) 55);
       Assert.fail("IOException expected.");
     } catch (IOException ioe) {
-      System.out.println("ok: "+ioe);
+      LOG.info("ok: " + ioe);
     }
-    
+
     try {
       harFileSystem.delete(fooPath, true);
       Assert.fail("IOException expected.");
     } catch (IOException ioe) {
-      System.out.println("ok: "+ioe);
+      LOG.info("ok: " + ioe);
     }
-    
+
     try {
       harFileSystem.mkdirs(fooPath, new FsPermission("+rwx"));
       Assert.fail("IOException expected.");
     } catch (IOException ioe) {
-      System.out.println("ok: "+ioe);
+      LOG.info("ok: " + ioe);
     }
-    
+
     final Path indexPath = new Path(harPath, "_index");
     try {
       harFileSystem.copyFromLocalFile(false, indexPath, fooPath);
       Assert.fail("IOException expected.");
     } catch (IOException ioe) {
-      System.out.println("ok: "+ioe);
+      LOG.info("ok: " + ioe);
     }
-    
+
     try {
       harFileSystem.startLocalOutput(fooPath, indexPath);
       Assert.fail("IOException expected.");
     } catch (IOException ioe) {
-      System.out.println("ok: "+ioe);
+      LOG.info("ok: " + ioe);
     }
-    
+
     try {
       harFileSystem.completeLocalOutput(fooPath, indexPath);
       Assert.fail("IOException expected.");
     } catch (IOException ioe) {
-      System.out.println("ok: "+ioe);
+      LOG.info("ok: " + ioe);
     }
-    
+
     try {
       harFileSystem.setOwner(fooPath, "user", "group");
       Assert.fail("IOException expected.");
     } catch (IOException ioe) {
-      System.out.println("ok: "+ioe);
+      LOG.info("ok: " + ioe);
     }
-    
+
     try {
       harFileSystem.setPermission(fooPath, new FsPermission("+x"));
       Assert.fail("IOException expected.");
     } catch (IOException ioe) {
-      System.out.println("ok: "+ioe);
+      LOG.info("ok: " + ioe);
     }
-  }  
-  
+  }
+
 }
