f532d3aef3b9c5468684de9d483473be8fcc6225
dennisyv <dennisyv@gmail.com>
2012-11-16 17:27:52 +0400

    Merge branch 'trunk' of https://github.com/griddynamics/yhadoop-common into gd-trunk
    Conflicts:
    	hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/TestCapacityScheduler.java

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/TestCapacityScheduler.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/TestCapacityScheduler.java
@@ -418,29 +418,6 @@ public class TestCapacityScheduler {
       B3_CAPACITY += B4_CAPACITY;
     }
   }
-  @Test
-  public void testCapacitySchedulerInfo() throws Exception {
-    QueueInfo queueInfo = resourceManager.getResourceScheduler().getQueueInfo("a", true, true);
-    Assert.assertEquals(queueInfo.getQueueName(), "a");
-    Assert.assertEquals(queueInfo.getChildQueues().size(), 2);
-
-    List<QueueUserACLInfo> userACLInfo = resourceManager.getResourceScheduler().getQueueUserAclInfo();
-    Assert.assertNotNull(userACLInfo);
-    for (QueueUserACLInfo queueUserACLInfo : userACLInfo) {
-      Assert.assertEquals(getQueueCount(userACLInfo, queueUserACLInfo.getQueueName()), 1);
-    }
-
-  }
-
-  private int getQueueCount(List<QueueUserACLInfo> queueInformation, String queueName) {
-    int result = 0;
-    for (QueueUserACLInfo queueUserACLInfo : queueInformation) {
-      if (queueName.equals(queueUserACLInfo.getQueueName())) {
-        result++;
-      }
-    }
-    return result;
-  }
 
   @Test
   public void testCapacitySchedulerInfo() throws Exception {
