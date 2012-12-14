2dc0e4a181ced3aed27ada326d073ee37d28727f
Ivan Veselovsky <iveselovsky@griddynamics.com>
2012-12-14 17:56:29 +0400

    HADOOP-9139: correction: -t option added to xargs to print out what is actually killed.

--- hadoop-common-project/hadoop-common/src/test/resources/kdc/killKdc.sh
+++ hadoop-common-project/hadoop-common/src/test/resources/kdc/killKdc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ps -ef | grep apacheds | grep -v grep | awk '{printf $2"\n"}' | xargs --no-run-if-empty kill -9
+ps -ef | grep apacheds | grep -v grep | awk '{printf $2"\n"}' | xargs -t --no-run-if-empty kill -9
 
