bcb4190248f5eaf083e1b310bb3eeab2f09e64b8
aleksgor <agorshkov@griddynamics.com>
2012-12-20 12:33:11 +0400

    add debug info

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapred/pipes/Application.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapred/pipes/Application.java
@@ -148,9 +148,14 @@ class Application<K1 extends WritableComparable, V1 extends Writable,
     
     downlink.authenticate(digestToSend, challenge);
     waitForAuthentication();
+    System.out.println("Authentication succeeded");
+
     LOG.debug("Authentication succeeded");
     downlink.start();
+    System.out.println("started");
+
     downlink.setJobConf(conf);
+    System.out.println("conf dounloaded");
   }
 
   private String getSecurityChallenge() {
