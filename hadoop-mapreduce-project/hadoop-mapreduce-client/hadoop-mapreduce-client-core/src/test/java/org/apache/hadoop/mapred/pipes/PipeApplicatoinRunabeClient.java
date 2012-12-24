e6a30ee4fcd592c4f50d7bf900e2297c77aecdf3
aleksgor <agorshkov@griddynamics.com>
2012-12-24 09:31:06 +0400

    add tests

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/test/java/org/apache/hadoop/mapred/pipes/PipeApplicatoinRunabeClient.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/test/java/org/apache/hadoop/mapred/pipes/PipeApplicatoinRunabeClient.java
@@ -11,17 +11,17 @@ import javax.crypto.SecretKey;
 
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.DataOutputBuffer;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.WritableUtils;
 import org.apache.hadoop.mapreduce.security.SecureShuffleUtils;
 import org.apache.hadoop.mapreduce.security.token.JobTokenSecretManager;
 
-public class PipeApplicatoinClient {
+public class PipeApplicatoinRunabeClient {
 
   public static void main(String[] args) {
-    PipeApplicatoinClient client = new PipeApplicatoinClient();
+    PipeApplicatoinRunabeClient client = new PipeApplicatoinRunabeClient();
     client.binaryProtocolStub();
   }
 
@@ -29,7 +29,7 @@ public class PipeApplicatoinClient {
     Socket socket =null;
     try {
 
-      int port = Integer
+      int port = Integer 
           .parseInt(System.getenv("mapreduce.pipes.command.port"));
       
       java.net.InetAddress addr = java.net.InetAddress.getLocalHost();
@@ -66,54 +66,52 @@ public class PipeApplicatoinClient {
 
       int j = WritableUtils.readVInt(dataInput);
       for (i = 0; i < j; i++) {
-        Text.readString(dataInput);
+        String key=Text.readString(dataInput);
         i++;
-         Text.readString(dataInput);
+        String val= Text.readString(dataInput);
+        System.out.println("key:"+key+"val:"+val);
       }
-      System.out.println("cfg 1s ok:" + ":"+System.currentTimeMillis());
+      System.out.println("cfg  222s ok:" + ":"+System.currentTimeMillis());
 
-      // output code
-      WritableUtils.writeVInt(dataout, 50);
-      IntWritable wt= new IntWritable();
-      wt.set(123);
-      writeObject(wt, dataout);
-      writeObject(new Text("value"), dataout);
+
+// RUN_MAP.code
+      //should be 3
+
+      i = WritableUtils.readVInt(dataInput);
       
-    //  PARTITIONED_OUTPUT
-      WritableUtils.writeVInt(dataout, 51);
-      WritableUtils.writeVInt(dataout, 0);
-      writeObject(wt, dataout);
-      writeObject(new Text("value"), dataout);
-
+      System.out.println("3=:" +i);
+      TestPipeApplication.FakeSplit split= new TestPipeApplication.FakeSplit() ; 
+      readObject(split, dataInput);
+      System.out.println("split=:" +split);
+      i = WritableUtils.readVInt(dataInput);
+      System.out.println("numReduces=:" +i);
+      i = WritableUtils.readVInt(dataInput);
+      System.out.println("pipedInput=:" +i);
       
-      // STATUS
+      //should be 2
+      
+      i = WritableUtils.readVInt(dataInput);
+      System.out.println("2=:" +i);
+      s= Text.readString(dataInput);
+      System.out.println("s=:" +s);
+      s= Text.readString(dataInput);
+      System.out.println("s=:" +s);
+/*
+      
+      System.out.println("start translate:" );
 
-      WritableUtils.writeVInt(dataout, 52);
-      Text.writeString(dataout, "PROGRESS");
-      dataout.flush();
-
-      // progress
-      WritableUtils.writeVInt(dataout, 53);
-      dataout.writeFloat(50.5f);
-      // register cunter
-      WritableUtils.writeVInt(dataout, 55);
-      // id
-      WritableUtils.writeVInt(dataout, 0);
-      Text.writeString(dataout, "group");
-      Text.writeString(dataout, "name");
-      // increment counter
-      System.out.println("10");
-      WritableUtils.writeVInt(dataout, 56);
-      WritableUtils.writeVInt(dataout, 0);
-
-      WritableUtils.writeVLong(dataout, 2);
-
+      for (int k=0;k<20;k++){
+        FloatWritable fw= new FloatWritable(k);
+        fw.write(dataout);
+        Text tx = new Text("k:"+k);
+         tx.write(dataout);
+      }
+      System.out.println("finish translate:" );
+   */
+      
       // done
-
       WritableUtils.writeVInt(dataout, 54);
-
       dataout.writeFloat(50.5f);
-      
       System.out.println("14");
   
       dataout.flush();
@@ -140,34 +138,61 @@ public class PipeApplicatoinClient {
 
   }
 
+  private void readObject(Writable obj , DataInputStream inStream)  throws IOException {
+    int numBytes = WritableUtils.readVInt(inStream);
+    byte[] buffer;
+    // For BytesWritable and Text, use the specified length to set the length
+    // this causes the "obvious" translations to work. So that if you emit
+    // a string "abc" from C++, it shows up as "abc".
+    if (obj instanceof BytesWritable) {
+      buffer = new byte[numBytes];
+      inStream.readFully(buffer);
+      ((BytesWritable) obj).set(buffer, 0, numBytes);
+    } else if (obj instanceof Text) {
+      buffer = new byte[numBytes];
+      inStream.readFully(buffer);
+      ((Text) obj).set(buffer);
+    } else {
+      obj.readFields(inStream);
+    }
+  }
+
+
   private void writeObject(Writable obj, DataOutputStream stream)
       throws IOException {
     // For Text and BytesWritable, encode them directly, so that they end up
     // in C++ as the natural translations.
     DataOutputBuffer buffer = new DataOutputBuffer();
+System.out.println("writeObject");
     if (obj instanceof Text) {
       Text t = (Text) obj;
       int len = t.getLength();
+      System.out.println("writeObject Text:"+len);
       WritableUtils.writeVLong(stream, len);
       stream.flush();
+      System.out.println("flush:"+System.currentTimeMillis());
 
       stream.write(t.getBytes(), 0, len);
       stream.flush();
+      System.out.println("flush2:"+System.currentTimeMillis());
 
     } else if (obj instanceof BytesWritable) {
       BytesWritable b = (BytesWritable) obj;
       int len = b.getLength();
+      System.out.println("writeObject BytesWritable:"+len);
       WritableUtils.writeVLong(stream, len);
       stream.write(b.getBytes(), 0, len);
     } else {
       buffer.reset();
       obj.write(buffer);
       int length = buffer.getLength();
+      System.out.println("writeObject :"+length);
 
       WritableUtils.writeVInt(stream, length);
       stream.write(buffer.getData(), 0, length);
     }
     stream.flush();
+    System.out.println("flush3:"+System.currentTimeMillis());
 
   }
 
--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/test/java/org/apache/hadoop/mapred/pipes/TestPipeApplication.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/test/java/org/apache/hadoop/mapred/pipes/TestPipeApplication.java
@@ -1,22 +1,21 @@
 package org.apache.hadoop.mapred.pipes;
 
-import java.io.ByteArrayOutputStream;
+import java.io.DataInput;
+import java.io.DataOutput;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.RawLocalFileSystem;
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapred.IFile.Writer;
@@ -35,7 +34,6 @@ import org.apache.hadoop.util.Progressable;
 import org.apache.hadoop.yarn.security.ApplicationTokenIdentifier;
 import org.junit.Test;
 
-
 import static org.junit.Assert.*;
 
 public class TestPipeApplication {
@@ -43,72 +41,71 @@ public class TestPipeApplication {
       TestPipeApplication.class.getName() + "-workSpace");
 
   @Test
+  public void testRunner() throws Exception {
+
+    File psw = new File("./jobTokenPassword");
+    if (psw.exists()) {
+      FileUtil.chmod(psw.getAbsolutePath(), "700");
+      psw.delete();
+    }
+    File psw1 = new File("./.jobTokenPassword.crc");
+    if (psw1.exists()) {
+      FileUtil.chmod(psw1.getAbsolutePath(), "700");
+      psw1.delete();
+    }
+    try {
+      RecordReader<FloatWritable, NullWritable> rReader = new Reader();
+      JobConf conf = new JobConf();
+      conf.set(Submitter.IS_JAVA_RR, "true");
+      conf.set(MRJobConfig.TASK_ATTEMPT_ID, "attempt_001_02_r03_04_05");
+
+      CombineOutputCollector<IntWritable, Text> output = new CombineOutputCollector<IntWritable, Text>(
+          new Counters.Counter(), new Progress(), conf);
+      FileSystem fs = new RawLocalFileSystem();
+      fs.setConf(conf);
+      Writer<IntWritable, Text> wr = new Writer<IntWritable, Text>(conf, fs,
+          new Path(workSpace + File.separator + "outfile"), IntWritable.class,
+          Text.class, null, null);
+      output.setWriter(wr);
+
+      File fCommand = getFileCommand("org.apache.hadoop.mapred.pipes.PipeApplicatoinRunabeClient");
+
+      conf.set(MRJobConfig.CACHE_LOCALFILES, fCommand.getAbsolutePath());
+
+      Token<ApplicationTokenIdentifier> token = new Token<ApplicationTokenIdentifier>(
+          "user".getBytes(), "password".getBytes(), new Text("kind"), new Text(
+              "service"));
+      conf.getCredentials().addToken(new Text("ShuffleAndJobToken"), token);
+      TestTaskReporter reporter = new TestTaskReporter();
+      PipesMapRunner<FloatWritable, NullWritable, IntWritable, Text> runner = new PipesMapRunner<FloatWritable, NullWritable, IntWritable, Text>();
+      runner.configure(conf);
+      runner.run(rReader, output, reporter);
+    } finally {
+      psw.deleteOnExit();
+      psw1.deleteOnExit();
+    }
+  }
+
+  @Test
   public void testOne() throws Throwable {
     JobConf conf = new JobConf();
 
-    RecordReader<FloatWritable, NullWritable> rReader = new RecordReader<FloatWritable, NullWritable>() {
-      private float index = 0.0f;
+    RecordReader<FloatWritable, NullWritable> rReader = new Reader();
 
-      @Override
-      public boolean next(FloatWritable key, NullWritable value)
-          throws IOException {
-        key.set(index++);
-        return false;
-      }
-
-      @Override
-      public float getProgress() throws IOException {
-        return index;
-      }
-
-      @Override
-      public long getPos() throws IOException {
-
-        return 0;
-      }
-
-      @Override
-      public NullWritable createValue() {
-
-        return NullWritable.get();
-      }
-
-      @Override
-      public FloatWritable createKey() {
-        FloatWritable result = new FloatWritable(index);
-        return result;
-      }
-
-      @Override
-      public void close() throws IOException {
-
-      }
-    };
-
-    String classpath = System.getProperty("java.class.path");
-    File fCommand = new File(workSpace + File.separator + "cache.sh");
-    fCommand.deleteOnExit();
-    if (!fCommand.getParentFile().exists()) {
-      fCommand.getParentFile().mkdirs();
-    }
-    fCommand.createNewFile();
-    OutputStream os = new FileOutputStream(fCommand);
-    os.write("#!/bin/sh \n".getBytes());
-
-    // /opt/yahoo/yhadoop-common/hadoop-common-project/hadoop-common
-    // /opt/yahoo/yhadoop-common/hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core
-
-    os.write(("java -cp " + classpath + " org.apache.hadoop.mapred.pipes.PipeApplicatoinClient")
-        .getBytes());
-    os.flush();
-    os.close();
+    File fCommand = getFileCommand("org.apache.hadoop.mapred.pipes.PipeApplicatoinClient");
 
     TestTaskReporter reporter = new TestTaskReporter();
-  
+
     File psw = new File("./jobTokenPassword");
-    psw.deleteOnExit();
-    psw = new File("./.jobTokenPassword.crc");
-    psw.deleteOnExit();
+    if (psw.exists()) {
+      FileUtil.chmod(psw.getAbsolutePath(), "700");
+      psw.delete();
+    }
+    File psw1 = new File("./.jobTokenPassword.crc");
+    if (psw1.exists()) {
+      FileUtil.chmod(psw1.getAbsolutePath(), "700");
+      psw1.delete();
+    }
     try {
 
       TaskAttemptID taskid = TaskAttemptID.forName("attempt_001_02_r03_04_05");
@@ -137,39 +134,54 @@ public class TestPipeApplication {
           Text.class, null, null);
       output.setWriter(wr);
       conf.set(Submitter.PRESERVE_COMMANDFILE, "true");
-      
+
       Application<WritableComparable<Object>, Writable, IntWritable, Text> application = new Application<WritableComparable<Object>, Writable, IntWritable, Text>(
           conf, rReader, output, reporter, IntWritable.class, Text.class);
       application.getDownlink().flush();
-      
-      
+
       application.waitForFinish();
-      
 
       wr.close();
-      
+
       assertEquals(1.0, reporter.getProgress(), 0.01);
       assertNotNull(reporter.getCounter("group", "name"));
       assertEquals(reporter.getStatus(), "PROGRESS");
-      
+
       application.getDownlink().close();
     } finally {
       psw.deleteOnExit();
-      psw.deleteOnExit();
+      psw1.deleteOnExit();
 
     }
     System.out.println("ok!");
   }
 
-  
   private class Progress implements Progressable {
 
     @Override
     public void progress() {
-      // no think todo
 
     }
 
+  }
+
+  private File getFileCommand( String clazz ) throws Exception {
+    String classpath = System.getProperty("java.class.path");
+    File fCommand = new File(workSpace + File.separator + "cache.sh");
+    fCommand.deleteOnExit();
+    if (!fCommand.getParentFile().exists()) {
+      fCommand.getParentFile().mkdirs();
+    }
+    fCommand.createNewFile();
+    OutputStream os = new FileOutputStream(fCommand);
+    os.write("#!/bin/sh \n".getBytes());
+
+    os.write(("java -cp " + classpath + " "+clazz)
+        .getBytes());
+    os.flush();
+    os.close();
+    FileUtil.chmod(fCommand.getAbsolutePath(), "700");
+    return fCommand;
   }
 
   private class CombineOutputCollector<K extends Object, V extends Object>
@@ -177,14 +189,11 @@ public class TestPipeApplication {
     private Writer<K, V> writer;
     private Counters.Counter outCounter;
     private Progressable progressable;
-    private long progressBar;
 
     public CombineOutputCollector(Counters.Counter outCounter,
         Progressable progressable, Configuration conf) {
       this.outCounter = outCounter;
       this.progressable = progressable;
-      progressBar = conf
-          .getLong(MRJobConfig.COMBINE_RECORDS_BEFORE_PROGRESS, 0);
     }
 
     public synchronized void setWriter(Writer<K, V> writer) {
@@ -198,11 +207,27 @@ public class TestPipeApplication {
     }
   }
 
+  public static class FakeSplit implements InputSplit {
+    public void write(DataOutput out) throws IOException {
+    }
+
+    public void readFields(DataInput in) throws IOException {
+    }
+
+    public long getLength() {
+      return 0L;
+    }
+
+    public String[] getLocations() {
+      return new String[0];
+    }
+  }
+
   private class TestTaskReporter implements Reporter {
     private int recordNum = 0; // number of records processed
     private String status = null;
     private Counters counters = new Counters();
-    private InputSplit split;
+    private InputSplit split = new FakeSplit();
 
     @Override
     public void progress() {
@@ -256,16 +281,50 @@ public class TestPipeApplication {
       return split;
     }
 
-    public void setInputSplit(InputSplit split)
-        throws UnsupportedOperationException {
-      this.split = split;
-    }
-
     @Override
     public float getProgress() {
       return recordNum;
     }
 
+  }
+
+  private class Reader implements RecordReader<FloatWritable, NullWritable> {
+    private float index = 0.0f;
+
+    @Override
+    public boolean next(FloatWritable key, NullWritable value)
+        throws IOException {
+      key.set(index++);
+      return false;
+    }
+
+    @Override
+    public float getProgress() throws IOException {
+      return index;
+    }
+
+    @Override
+    public long getPos() throws IOException {
+
+      return 0;
+    }
+
+    @Override
+    public NullWritable createValue() {
+
+      return NullWritable.get();
+    }
+
+    @Override
+    public FloatWritable createKey() {
+      FloatWritable result = new FloatWritable(index);
+      return result;
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
   }
 
 }
