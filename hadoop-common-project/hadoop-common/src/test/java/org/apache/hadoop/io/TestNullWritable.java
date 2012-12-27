dc4c0547397bb87adc890be30e058e7c1e35561a
haghard <haghard84@gmail.com>
2012-12-27 19:31:56 +0400

    YAHOO-202 -- QE add test classes TestNullWritable and test methods in TestText

---
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/io/TestNullWritable.java
@@ -0,0 +1,12 @@
+package org.apache.hadoop.io;
+
+import junit.framework.TestCase;
+
+public class TestNullWritable extends TestCase {
+    
+  public void testNullableWritable() {    
+    NullWritable nullWritable = NullWritable.get();
+    assertTrue("testNullableWritable equals error!!!", nullWritable.equals(NullWritable.get()));
+    assertNotNull("testNullableWritable null error!!!", WritableComparator.get(NullWritable.class));    
+  }
+}
