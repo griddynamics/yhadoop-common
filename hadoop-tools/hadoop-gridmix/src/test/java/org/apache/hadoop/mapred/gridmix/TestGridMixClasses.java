eddc81057af931d724e1ca1eea528895370341dd
aleksgor <agorshkov@griddynamics.com>
2013-01-28 12:39:38 +0400

    add compare tests

--- hadoop-tools/hadoop-gridmix/src/test/java/org/apache/hadoop/mapred/gridmix/TestGridMixClasses.java
+++ hadoop-tools/hadoop-gridmix/src/test/java/org/apache/hadoop/mapred/gridmix/TestGridMixClasses.java
@@ -12,8 +12,8 @@ import org.apache.hadoop.CustomOutputCommitter;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.WritableUtils;
 import org.apache.hadoop.mapred.JobContext;
-import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.MRJobConfig;
 import org.apache.hadoop.mapreduce.MapContext;
@@ -23,15 +23,14 @@ import org.apache.hadoop.mapreduce.RecordWriter;
 import org.apache.hadoop.mapreduce.StatusReporter;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.TaskAttemptID;
-import org.apache.hadoop.mapreduce.TaskCounter;
 import org.apache.hadoop.mapreduce.Mapper.Context;
-import org.apache.hadoop.mapreduce.counters.GenericCounter;
 import org.apache.hadoop.mapreduce.lib.input.CombineFileSplit;
 import org.apache.hadoop.mapreduce.lib.map.WrappedMapper;
 import org.apache.hadoop.mapreduce.task.MapContextImpl;
 import org.apache.hadoop.mapreduce.task.TaskAttemptContextImpl;
 import org.apache.hadoop.tools.rumen.ResourceUsageMetrics;
 import org.junit.Test;
+
 
 import static org.junit.Assert.*;
 
@@ -40,8 +39,8 @@ public class TestGridMixClasses {
   @Test
   public void testLoadSplit() throws Exception {
 
-  LoadSplit test= getLoadSplit();
-   
+    LoadSplit test = getLoadSplit();
+
     ByteArrayOutputStream data = new ByteArrayOutputStream();
     DataOutputStream out = new DataOutputStream(data);
     test.write(out);
@@ -107,48 +106,46 @@ public class TestGridMixClasses {
   @SuppressWarnings({ "rawtypes", "unchecked" })
   @Test
   public void test1() throws Exception {
-      
-     Configuration conf= new Configuration(); 
-     conf.setInt(JobContext.NUM_REDUCES,2);
-     
+
+    Configuration conf = new Configuration();
+    conf.setInt(JobContext.NUM_REDUCES, 2);
+
     CompressionEmulationUtil.setCompressionEmulationEnabled(conf, true);
     conf.setBoolean(MRJobConfig.MAP_OUTPUT_COMPRESS, true);
-    
+
     TaskAttemptID taskid = new TaskAttemptID();
     RecordReader reader = new FakeRecordReader();
-   
-    LoadRecordWriter  writer= new  LoadRecordWriter();
-    
-    OutputCommitter committer= new CustomOutputCommitter();
+
+    LoadRecordWriter writer = new LoadRecordWriter();
+
+    OutputCommitter committer = new CustomOutputCommitter();
     StatusReporter reporter = new TaskAttemptContextImpl.DummyReporter();
-        LoadSplit split= getLoadSplit();
-    
-    MapContext mapcontext= new MapContextImpl(conf, taskid, reader, writer, committer, reporter, split);
+    LoadSplit split = getLoadSplit();
+
+    MapContext mapcontext = new MapContextImpl(conf, taskid, reader, writer,
+        committer, reporter, split);
     Context ctxt = new WrappedMapper().getMapContext(mapcontext);
-    
+
     reader.initialize(split, ctxt);
     ctxt.getConfiguration().setBoolean(MRJobConfig.MAP_OUTPUT_COMPRESS, true);
-    CompressionEmulationUtil.setCompressionEmulationEnabled(ctxt.getConfiguration(), true);
-    
-    Counter counter = new GenericCounter(TaskCounter.SPILLED_RECORDS.toString(),TaskCounter.SPILLED_RECORDS.toString(),33);
-    
-    
-  //  when(ctxt.getCounter(TaskCounter.SPILLED_RECORDS)).thenReturn(counter);
+    CompressionEmulationUtil.setCompressionEmulationEnabled(
+        ctxt.getConfiguration(), true);
 
-    LoadJob.LoadMapper mapper= new  LoadJob.LoadMapper();
-      
-     mapper.run(ctxt);
-     
- //    mapper.cleanup(ctxt);
-    System.out.println("OK");  
-    Map<Object,Object> data=writer.getData();
-    assertEquals(2,data.size());
-    
-    
+    // when(ctxt.getCounter(TaskCounter.SPILLED_RECORDS)).thenReturn(counter);
+
+    LoadJob.LoadMapper mapper = new LoadJob.LoadMapper();
+
+    mapper.run(ctxt);
+
+    // mapper.cleanup(ctxt);
+    System.out.println("OK");
+    Map<Object, Object> data = writer.getData();
+    assertEquals(2, data.size());
+
   }
-  private LoadSplit getLoadSplit() throws Exception{
-    
-    
+
+  private LoadSplit getLoadSplit() throws Exception {
+
     Path[] files = { new Path("one"), new Path("two") };
     long[] start = { 1, 2 };
     long[] lengths = { 100, 200 };
@@ -165,53 +162,56 @@ public class TestGridMixClasses {
     long[] reduceOutputBytes = { 101L, 102L };
     long[] reduceOutputRecords = { 111L, 112L };
 
-    LoadSplit result = new LoadSplit(cfsplit, 2, 1, 4L, 5L, 6L, 7L, reduceBytes,
-        reduceRecords, reduceOutputBytes, reduceOutputRecords, metrics,
-        rMetrics);
+    LoadSplit result = new LoadSplit(cfsplit, 2, 1, 4L, 5L, 6L, 7L,
+        reduceBytes, reduceRecords, reduceOutputBytes, reduceOutputRecords,
+        metrics, rMetrics);
     return result;
   }
-  
-  protected class FakeRecordReader extends RecordReader{
 
-    int counter=10; 
+  protected class FakeRecordReader extends
+      RecordReader<NullWritable, GridmixRecord> {
+
+    int counter = 10;
+
     @Override
     public void initialize(InputSplit split, TaskAttemptContext context)
         throws IOException, InterruptedException {
-     
-      
+
     }
 
     @Override
     public boolean nextKeyValue() throws IOException, InterruptedException {
       counter--;
-      return counter>0;
+      return counter > 0;
     }
 
     @Override
-    public Object getCurrentKey() throws IOException, InterruptedException {
-      
-      return  NullWritable.get();
+    public NullWritable getCurrentKey() throws IOException,
+        InterruptedException {
+
+      return NullWritable.get();
     }
 
     @Override
-    public Object getCurrentValue() throws IOException, InterruptedException {
-      return new GridmixRecord(100,100L);
+    public GridmixRecord getCurrentValue() throws IOException,
+        InterruptedException {
+      return new GridmixRecord(100, 100L);
     }
 
     @Override
     public float getProgress() throws IOException, InterruptedException {
-      return counter/10.0f;
+      return counter / 10.0f;
     }
 
     @Override
     public void close() throws IOException {
-      counter=10;
+      counter = 10;
     }
   }
-  
-  
- private class  LoadRecordWriter extends RecordWriter {
-    private Map<Object, Object> data= new HashMap<Object, Object>();
+
+  private class LoadRecordWriter extends RecordWriter<Object, Object> {
+    private Map<Object, Object> data = new HashMap<Object, Object>();
+
     @Override
     public void write(Object key, Object value) throws IOException,
         InterruptedException {
@@ -222,10 +222,64 @@ public class TestGridMixClasses {
     public void close(TaskAttemptContext context) throws IOException,
         InterruptedException {
     }
-    
-    public Map<Object, Object> getData(){
+
+    public Map<Object, Object> getData() {
       return data;
     }
-    
+
   };
+
+  @Test
+  public void testLoadJobLoadSortComparator() throws Exception {
+    LoadJob.LoadSortComparator test = new LoadJob.LoadSortComparator();
+
+    ByteArrayOutputStream data = new ByteArrayOutputStream();
+    DataOutputStream dos = new DataOutputStream(data);
+    WritableUtils.writeVInt(dos, 2);
+    WritableUtils.writeVInt(dos, 1);
+    WritableUtils.writeVInt(dos, 4);
+    WritableUtils.writeVInt(dos, 7);
+    WritableUtils.writeVInt(dos, 4);
+
+    byte[] b1 = data.toByteArray();
+
+    byte[] b2 = data.toByteArray();
+
+    // int s1, int l1, byte[] b2, int s2, int l2
+    assertEquals(0, test.compare(b1, 0, 1, b2, 0, 1));
+    b2[2] = 5;
+    assertEquals(-1, test.compare(b1, 0, 1, b2, 0, 1));
+    b2[2] = 2;
+    assertEquals(2, test.compare(b1, 0, 1, b2, 0, 1));
+    // compare arrays by first byte witch offset (2-1) because 4==4
+    b2[2] = 4;
+    assertEquals(1, test.compare(b1, 0, 1, b2, 1, 1));
+
+  }
+  
+  @Test
+  public void testGridmixJobSpecGroupingComparator () throws Exception {
+    GridmixJob.SpecGroupingComparator  test = new GridmixJob.SpecGroupingComparator ();
+
+    ByteArrayOutputStream data = new ByteArrayOutputStream();
+    DataOutputStream dos = new DataOutputStream(data);
+    WritableUtils.writeVInt(dos, 2);
+    WritableUtils.writeVInt(dos, 1);
+    WritableUtils.writeVInt(dos, 4);
+    WritableUtils.writeVInt(dos, 7);
+    WritableUtils.writeVInt(dos, 4);
+
+    byte[] b1 = data.toByteArray();
+
+    byte[] b2 = data.toByteArray();
+
+    // int s1, int l1, byte[] b2, int s2, int l2
+    assertEquals(0, test.compare(b1, 0, 1, b2, 0, 1));
+    b2[2] = 5;
+    assertEquals(0, test.compare(b1, 0, 1, b2, 0, 1));
+    // by Reduce spec
+    b2[2] = 0;
+    assertEquals(4, test.compare(b1, 0, 1, b2, 0, 1));
+  }
+ 
 }
