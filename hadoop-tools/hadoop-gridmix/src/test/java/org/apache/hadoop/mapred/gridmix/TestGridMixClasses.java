f46ef2a3d1cc9829b87ed04644545394acd56b7d
aleksgor <agorshkov@griddynamics.com>
2013-01-29 11:42:34 +0400

    add test

--- hadoop-tools/hadoop-gridmix/src/test/java/org/apache/hadoop/mapred/gridmix/TestGridMixClasses.java
+++ hadoop-tools/hadoop-gridmix/src/test/java/org/apache/hadoop/mapred/gridmix/TestGridMixClasses.java
@@ -5,6 +5,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,9 +33,10 @@ import org.apache.hadoop.mapreduce.task.TaskAttemptContextImpl;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.tools.rumen.JobStory;
 import org.apache.hadoop.tools.rumen.ResourceUsageMetrics;
+import org.apache.hadoop.yarn.YarnException;
+import org.jets3t.service.io.InputStreamWrapper;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
-
 
 import static org.junit.Assert.*;
 
@@ -260,10 +262,10 @@ public class TestGridMixClasses {
     assertEquals(1, test.compare(b1, 0, 1, b2, 1, 1));
 
   }
-  
+
   @Test
-  public void testGridmixJobSpecGroupingComparator () throws Exception {
-    GridmixJob.SpecGroupingComparator  test = new GridmixJob.SpecGroupingComparator ();
+  public void testGridmixJobSpecGroupingComparator() throws Exception {
+    GridmixJob.SpecGroupingComparator test = new GridmixJob.SpecGroupingComparator();
 
     ByteArrayOutputStream data = new ByteArrayOutputStream();
     DataOutputStream dos = new DataOutputStream(data);
@@ -284,33 +286,103 @@ public class TestGridMixClasses {
     // by Reduce spec
     b2[2] = 1;
     assertEquals(-1, test.compare(b1, 0, 1, b2, 0, 1));
-    
-   
-    assertEquals(0,test.compare(new GridmixKey(GridmixKey.DATA,100,2), new GridmixKey(GridmixKey.DATA,100,2)));
+
+    assertEquals(0, test.compare(new GridmixKey(GridmixKey.DATA, 100, 2),
+        new GridmixKey(GridmixKey.DATA, 100, 2)));
     // REDUSE SPEC
-    assertEquals(-1,test.compare(new GridmixKey(GridmixKey.REDUCE_SPEC,100,2), new GridmixKey(GridmixKey.DATA,100,2)));
-    assertEquals(1,test.compare(new GridmixKey(GridmixKey.DATA,100,2), new GridmixKey(GridmixKey.REDUCE_SPEC,100,2)));
+    assertEquals(-1, test.compare(
+        new GridmixKey(GridmixKey.REDUCE_SPEC, 100, 2), new GridmixKey(
+            GridmixKey.DATA, 100, 2)));
+    assertEquals(1, test.compare(new GridmixKey(GridmixKey.DATA, 100, 2),
+        new GridmixKey(GridmixKey.REDUCE_SPEC, 100, 2)));
     // only DATA
-    assertEquals(2,test.compare(new GridmixKey(GridmixKey.DATA,102,2), new GridmixKey(GridmixKey.DATA,100,2)));
+    assertEquals(2, test.compare(new GridmixKey(GridmixKey.DATA, 102, 2),
+        new GridmixKey(GridmixKey.DATA, 100, 2)));
 
   }
-  
+
   @Test
-  public void testCompareGridmixJob() throws Exception{
-    Configuration conf= new Configuration();
-    Path outRoot= new Path("target");
+  public void testCompareGridmixJob() throws Exception {
+    Configuration conf = new Configuration();
+    Path outRoot = new Path("target");
     JobStory jobdesc = mock(JobStory.class);
     when(jobdesc.getName()).thenReturn("JobName");
     when(jobdesc.getJobConf()).thenReturn(new JobConf(conf));
-    UserGroupInformation ugi=  UserGroupInformation.getCurrentUser();
-    //final Configuration conf, long submissionMillis,
-    //final JobStory jobdesc, Path outRoot, UserGroupInformation ugi, 
-    //final int seq
-    
-    GridmixJob j1= new LoadJob(conf, 1000L,jobdesc,outRoot,ugi,0);
-    GridmixJob j2= new LoadJob(conf, 1000L,jobdesc,outRoot,ugi,0);
+    UserGroupInformation ugi = UserGroupInformation.getCurrentUser();
+    // final Configuration conf, long submissionMillis,
+    // final JobStory jobdesc, Path outRoot, UserGroupInformation ugi,
+    // final int seq
+
+    GridmixJob j1 = new LoadJob(conf, 1000L, jobdesc, outRoot, ugi, 0);
+    GridmixJob j2 = new LoadJob(conf, 1000L, jobdesc, outRoot, ugi, 0);
     assertTrue(j1.equals(j2));
     assertEquals(0, j1.compareTo(j2));
   }
-  
+/*
+ * test ReadRecordFactory hould read all data from inputstream 
+ */
+  @Test
+  public void testReadRecordFactory() throws Exception{
+    
+    // RecordFactory factory, InputStream src,    Configuration conf
+    RecordFactory rf= new FakeRecordFactory();
+    FakeInputStream input= new  FakeInputStream();
+    ReadRecordFactory test= new ReadRecordFactory(rf,input,new Configuration()) ;
+    GridmixKey key=new GridmixKey(GridmixKey.DATA, 100, 2);
+    GridmixRecord val= new GridmixRecord(200,2);
+    while( test.next(key, val)){
+      
+    }
+    // should be read 10* (GridmixKey.size +GridmixRecord.value)
+    assertEquals(3000, input.getCounter());
+    // shoutd be 0;
+    assertEquals(0, rf.getProgress(),0.01);
+
+    System.out.println("dd:"+input.getCounter());
+    System.out.println("rf:"+rf.getProgress());
+    test.close();
+  }
+
+  private class FakeRecordFactory extends RecordFactory {
+
+    private int counter = 10;
+
+    @Override
+    public void close() throws IOException {
+
+    }
+
+    @Override
+    public boolean next(GridmixKey key, GridmixRecord val) throws IOException {
+      counter--;
+      return counter>=0;
+    }
+
+    @Override
+    public float getProgress() throws IOException {
+      return counter;
+    }
+   
+  }
+
+  private class FakeInputStream extends InputStream {
+    private long counter;
+    @Override
+    public int read() throws IOException {
+      return 0;
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+      int realLen = len - off;
+      counter+=realLen;
+      for (int i = 0; i < b.length; i++) {
+        b[i] = 0;
+      }
+      return realLen;
+    }
+public long getCounter(){
+  return counter;
+}
+  }
 }
