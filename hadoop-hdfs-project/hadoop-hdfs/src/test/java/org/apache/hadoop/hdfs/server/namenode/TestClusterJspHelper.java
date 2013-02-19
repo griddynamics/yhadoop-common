73b4a456e35732c582cded69881ca18c32c4b6f2
vbondarev <vbondarev@griddynamics.com>
2013-02-19 18:58:38 +0400

    YAHOO - 234 QE remove ext parameter from hdfs-site.xml and add it in the TestClusterJspHelper

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/TestClusterJspHelper.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/TestClusterJspHelper.java
@@ -1,5 +1,5 @@
 /**
-* Licensed to the Apache Software Foundation (ASF) under one
+ * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
@@ -18,41 +18,85 @@
 package org.apache.hadoop.hdfs.server.namenode;
 
 import static org.junit.Assert.*;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.Writer;
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hdfs.MiniDFSCluster;
 import org.apache.hadoop.hdfs.MiniDFSNNTopology;
 import org.apache.hadoop.hdfs.server.namenode.ClusterJspHelper.ClusterStatus;
 import org.apache.hadoop.hdfs.server.namenode.ClusterJspHelper.DecommissionStatus;
+import org.apache.hadoop.io.DataInputBuffer;
+import org.apache.hadoop.io.DataOutputBuffer;
+import org.apache.hadoop.io.Text;
+import org.apache.log4j.Logger;
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.google.common.io.Files;
+
 public class TestClusterJspHelper {
+  
+  private MiniDFSCluster cluster;
+  private final int dataNodeNumber = 2;
+  private final int nameNodePort = 45541;
+  private final int nameNodeHttpPort = 50070;
+  private URL originFileUrl;
+  private File originFile;
+  private File renamedFile;
+  private static final String ORIGINAL_FILE_NAME = "hdfs-site.xml";
+  private static final String RENAMED_FILE_NAME = "hdfs-site_.xml";
+  private static final String FAKE_FILE_CONTEXT = "<?xml version=\"1.0\"?> <?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?>"
+      + "<configuration> <property> <name>fs.defaultFS</name> <value>hdfs://localhost.localdomain:45541</value> <description></description> </property> "
+      + "<property> <name>hadoop.security.authentication</name> <value>simple</value> </property>" 
+      + " </configuration>";
 
-  private static MiniDFSCluster cluster;
-  private static Configuration conf = new Configuration();
-  private static final int dataNodeNumber = 2;
-  private static final int nameNodePort = 45541;
-  private static final int nameNodeHttpPort = 50070;
+  private static final Logger logger = Logger
+      .getLogger(TestClusterJspHelper.class);
 
-  @BeforeClass
-  public static void before() throws IOException {
-    cluster = new MiniDFSCluster.Builder(conf)
-        .nnTopology(
-            MiniDFSNNTopology.simpleSingleNN(nameNodePort, nameNodeHttpPort))
-        .numDataNodes(dataNodeNumber).build();
-    cluster.waitClusterUp();
-  }
-
-  @AfterClass
-  public static void after() {
-    cluster.shutdown();
+  @SuppressWarnings("static-access")
+  private void renameConfigFile() throws IOException {    
+    originFileUrl = getClass().getClassLoader().getSystemResource(
+        ORIGINAL_FILE_NAME);
+    originFile = new File(originFileUrl.getPath());
+    if (originFile.exists()) {
+      String originPath = originFileUrl.getPath();
+      String renamedFilePath = new String(originPath.substring(0,
+          originPath.lastIndexOf("/"))
+          + "/" + RENAMED_FILE_NAME);
+      renamedFile = new File(renamedFilePath);
+      assertTrue("file rename fail ", originFile.renameTo(renamedFile));
+      Writer out = new BufferedWriter(new FileWriter(new File(originPath)));
+      out.write(FAKE_FILE_CONTEXT);
+      out.close();
+    }
   }
 
   @Test
   public void testClusterJspHelperReports() {
     try {
+      renameConfigFile();
+      Configuration conf = new Configuration();
+      cluster = new MiniDFSCluster.Builder(conf)
+          .nnTopology(
+              MiniDFSNNTopology.simpleSingleNN(nameNodePort, nameNodeHttpPort))
+          .numDataNodes(dataNodeNumber).build();
+      cluster.waitClusterUp();
+
       ClusterJspHelper clusterJspHelper = new ClusterJspHelper();
       ClusterStatus clusterStatus = clusterJspHelper
           .generateClusterHealthReport();
@@ -68,7 +112,15 @@ public class TestClusterJspHelper {
           "testClusterJspHelperReports wrong DecommissionStatus clusterid",
           decommissionStatus.clusterid.equals("testClusterID"));
     } catch (Exception ex) {
-      fail("testDefaultNamenode ex error !!!" + ex);
+      fail("testClusterJspHelperReports error " + ex);
+    } finally {
+      if (renamedFile != null & renamedFile.exists()) {
+        try {
+          Files.move(renamedFile, originFile);
+        } catch (IOException e) {
+          logger.error("fail rename config file");
+        }
+      }
     }
-  }    
+  }
 }
