bf154ad8edcfe5ea2fe20e2d3bca85d035fa14c2
aleksgor <agorshkov@griddynamics.com>
2013-02-20 15:13:27 +0400

    first test

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/ipc/TestSocketFactory.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/ipc/TestSocketFactory.java
@@ -17,8 +17,21 @@
  */
 package org.apache.hadoop.ipc;
 
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Proxy.Type;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,10 +41,13 @@ import junit.framework.Assert;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.net.NetUtils;
+import org.apache.hadoop.net.SocksSocketFactory;
 import org.apache.hadoop.net.StandardSocketFactory;
 import org.junit.Test;
 
 public class TestSocketFactory {
+
+  private volatile int port;
 
   @Test
   public void testSocketFactoryAsKeyInMap() throws Exception {
@@ -63,9 +79,122 @@ public class TestSocketFactory {
   }
 
   /**
-   * A dummy socket factory class that extends the StandardSocketFactory. 
+   * A dummy socket factory class that extends the StandardSocketFactory.
    */
   static class DummySocketFactory extends StandardSocketFactory {
-    
+
   }
+
+  @Test
+  public void testSocksSocketFactory() throws Exception {
+    ServerThread serverThread = new ServerThread();
+    Thread server = new Thread(serverThread);
+    server.start();
+    while (!serverThread.isReady()) {
+      Thread.sleep(10);
+    }
+    try {
+
+      InetAddress addr = InetAddress.getLocalHost();
+      SocksSocketFactory socketFactory = new SocksSocketFactory();
+      Socket socket = socketFactory.createSocket(addr, port);
+      checkSoket(socket);
+      socket.close();
+
+      socket = socketFactory.createSocket(addr, port,
+          InetAddress.getLocalHost(), 0);
+      checkSoket(socket);
+      socket.close();
+
+      socket = socketFactory.createSocket("localhost", port);
+      checkSoket(socket);
+      socket.close();
+
+      socket = socketFactory.createSocket("localhost", port,
+          InetAddress.getLocalHost(), 0);
+      checkSoket(socket);
+      socket.close();
+
+    } finally {
+      serverThread.stop();
+    }
+  }
+
+  @Test
+  public void testProxy() throws Exception {
+    SocksSocketFactory templateWithoutProxy = new SocksSocketFactory();
+    Proxy proxy = new Proxy(Type.SOCKS, InetSocketAddress.createUnresolved(
+        "localhost", 0));
+
+    SocksSocketFactory templateWithProxy = new SocksSocketFactory(proxy);
+    assertFalse(templateWithoutProxy.equals(templateWithProxy));
+
+    Configuration conf = new Configuration();
+    conf.set("hadoop.socks.server", "localhost:0");
+
+    templateWithoutProxy.setConf(conf);
+    assertTrue(templateWithoutProxy.equals(templateWithProxy));
+
+  }
+
+  private void checkSoket(Socket socket) throws Exception {
+    BufferedReader input = new BufferedReader(new InputStreamReader(
+        socket.getInputStream()));
+    DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+    out.writeBytes("test\n");
+    String answer = input.readLine();
+
+    System.out.println("ss:" + answer);
+    assertEquals("TEST", answer);
+
+  }
+
+  private class ServerThread implements Runnable {
+
+    private volatile boolean works = true;
+    private ServerSocket testSocket;
+    private volatile boolean ready = false;
+
+    @Override
+    public void run() {
+      try {
+        testSocket = new ServerSocket(0);
+        port = testSocket.getLocalPort();
+        System.out.println("port:" + port);
+        ready = true;
+        while (works) {
+          try {
+            Socket connectionSocket = testSocket.accept();
+            BufferedReader input = new BufferedReader(new InputStreamReader(
+                connectionSocket.getInputStream()));
+            DataOutputStream out = new DataOutputStream(
+                connectionSocket.getOutputStream());
+            String inData = input.readLine();
+
+            String outData = inData.toUpperCase() + "\n";
+            out.writeBytes(outData);
+          } catch (SocketException e) {
+            ;
+          }
+        }
+      } catch (IOException e) {
+        ;
+      }
+
+    }
+
+    public void stop() {
+      try {
+        testSocket.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      works = false;
+    }
+
+    public boolean isReady() {
+      return ready;
+    }
+  }
+
 }
