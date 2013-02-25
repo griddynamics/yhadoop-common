114d075988e7a7a179dad371c54986dde2bcafc7
aleksgor <agorshkov@griddynamics.com>
2013-02-25 12:17:43 +0400

    add tests

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-client/src/test/java/org/apache/hadoop/yarn/client/TestRMAdminProtocolPBClientImpl.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-client/src/test/java/org/apache/hadoop/yarn/client/TestRMAdminProtocolPBClientImpl.java
@@ -6,12 +6,21 @@ import java.lang.reflect.UndeclaredThrowableException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.yarn.api.ClientRMProtocol;
 import org.apache.hadoop.yarn.api.impl.pb.client.ClientRMProtocolPBClientImpl;
 import org.apache.hadoop.yarn.api.protocolrecords.CancelDelegationTokenRequest;
+import org.apache.hadoop.yarn.api.protocolrecords.GetAllApplicationsRequest;
+import org.apache.hadoop.yarn.api.protocolrecords.GetApplicationReportRequest;
+import org.apache.hadoop.yarn.api.protocolrecords.GetClusterMetricsRequest;
+import org.apache.hadoop.yarn.api.protocolrecords.GetClusterNodesRequest;
+import org.apache.hadoop.yarn.api.protocolrecords.GetDelegationTokenRequest;
+import org.apache.hadoop.yarn.api.protocolrecords.GetNewApplicationRequest;
+import org.apache.hadoop.yarn.api.protocolrecords.GetQueueInfoRequest;
+import org.apache.hadoop.yarn.api.protocolrecords.GetQueueUserAclsInfoRequest;
+import org.apache.hadoop.yarn.api.protocolrecords.KillApplicationRequest;
+import org.apache.hadoop.yarn.api.protocolrecords.RenewDelegationTokenRequest;
+import org.apache.hadoop.yarn.api.protocolrecords.SubmitApplicationRequest;
 import org.apache.hadoop.yarn.api.records.DelegationToken;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.exceptions.YarnRemoteException;
@@ -46,6 +55,7 @@ import org.apache.hadoop.yarn.proto.YarnServiceProtos.SubmitApplicationResponseP
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.google.protobuf.GeneratedMessage;
 import com.google.protobuf.RpcController;
 import com.google.protobuf.ServiceException;
 
@@ -56,14 +66,12 @@ import static org.junit.Assert.*;
  * 
  */
 public class TestRMAdminProtocolPBClientImpl {
-  private static final Log LOG = LogFactory
-      .getLog(TestRMAdminProtocolPBClientImpl.class);
   private final RecordFactory recordFactory = RecordFactoryProvider
       .getRecordFactory(null);
 
   private static ClientRMProtocol client;
 
-  private static int responce = 0;
+  private static int resultFlag = 0;
 
   /**
    * Start resource manager server
@@ -90,31 +98,360 @@ public class TestRMAdminProtocolPBClientImpl {
         .newRecordInstance(CancelDelegationTokenRequest.class);
     request.setDelegationToken(getDelegationToken());
 
-    responce = 0;
+    resultFlag = 0;
     assertNotNull(client.cancelDelegationToken(request));
-    responce = 1;
+    resultFlag = 1;
     try {
       client.cancelDelegationToken(request);
       fail("should be ServiceException ");
     } catch (YarnRemoteException e) {
       assertEquals("YarnRemoteException test message", e.getMessage());
     }
-    responce = 2;
+    resultFlag = 2;
     try {
       client.cancelDelegationToken(request);
       fail("should be UndeclaredThrowableException ");
     } catch (UndeclaredThrowableException e) {
-      
-    }
-    responce = 3;
-    try {
-      client.cancelDelegationToken(request);
-      fail("should be UndeclaredThrowableException ");
-    } catch (UndeclaredThrowableException e) {
-      
-    }
-    
 
+    }
+    resultFlag = 3;
+    try {
+      client.cancelDelegationToken(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+  }
+
+  @Test
+  public void testRenewDelegationToken() throws YarnRemoteException {
+    RenewDelegationTokenRequest request = recordFactory
+        .newRecordInstance(RenewDelegationTokenRequest.class);
+    request.setDelegationToken(getDelegationToken());
+
+    resultFlag = 0;
+    assertNotNull(client.renewDelegationToken(request));
+    resultFlag = 1;
+    try {
+      client.renewDelegationToken(request);
+      fail("should be ServiceException ");
+    } catch (YarnRemoteException e) {
+      assertEquals("YarnRemoteException test message", e.getMessage());
+    }
+    resultFlag = 2;
+    try {
+      client.renewDelegationToken(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+    resultFlag = 3;
+    try {
+      client.renewDelegationToken(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+  }
+
+  @Test
+  public void testGetDelegationToken() throws YarnRemoteException {
+    GetDelegationTokenRequest request = recordFactory
+        .newRecordInstance(GetDelegationTokenRequest.class);
+
+    resultFlag = 0;
+    assertNotNull(client.getDelegationToken(request));
+    resultFlag = 1;
+    try {
+      client.getDelegationToken(request);
+      fail("should be ServiceException ");
+    } catch (YarnRemoteException e) {
+      assertEquals("YarnRemoteException test message", e.getMessage());
+    }
+    resultFlag = 2;
+    try {
+      client.getDelegationToken(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+    resultFlag = 3;
+    try {
+      client.getDelegationToken(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+  }
+
+  @Test
+  public void testGetQueueUserAcls() throws YarnRemoteException {
+    GetQueueUserAclsInfoRequest request = recordFactory
+        .newRecordInstance(GetQueueUserAclsInfoRequest.class);
+
+    resultFlag = 0;
+    assertNotNull(client.getQueueUserAcls(request));
+    resultFlag = 1;
+    try {
+      client.getQueueUserAcls(request);
+      fail("should be ServiceException ");
+    } catch (YarnRemoteException e) {
+      assertEquals("YarnRemoteException test message", e.getMessage());
+    }
+    resultFlag = 2;
+    try {
+      client.getQueueUserAcls(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+    resultFlag = 3;
+    try {
+      client.getQueueUserAcls(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+  }
+
+  @Test
+  public void testGetQueueInfo() throws YarnRemoteException {
+    GetQueueInfoRequest request = recordFactory
+        .newRecordInstance(GetQueueInfoRequest.class);
+
+    resultFlag = 0;
+    assertNotNull(client.getQueueInfo(request));
+    resultFlag = 1;
+    try {
+      client.getQueueInfo(request);
+      fail("should be ServiceException ");
+    } catch (YarnRemoteException e) {
+      assertEquals("YarnRemoteException test message", e.getMessage());
+    }
+    resultFlag = 2;
+    try {
+      client.getQueueInfo(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+    resultFlag = 3;
+    try {
+      client.getQueueInfo(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+  }
+
+  @Test
+  public void testGetClusterNodes() throws YarnRemoteException {
+    GetClusterNodesRequest request = recordFactory
+        .newRecordInstance(GetClusterNodesRequest.class);
+
+    resultFlag = 0;
+    assertNotNull(client.getClusterNodes(request));
+    resultFlag = 1;
+    try {
+      client.getClusterNodes(request);
+      fail("should be ServiceException ");
+    } catch (YarnRemoteException e) {
+      assertEquals("YarnRemoteException test message", e.getMessage());
+    }
+    resultFlag = 2;
+    try {
+      client.getClusterNodes(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+    resultFlag = 3;
+    try {
+      client.getClusterNodes(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+  }
+
+  @Test
+  public void testGetAllApplications() throws YarnRemoteException {
+    GetAllApplicationsRequest request = recordFactory
+        .newRecordInstance(GetAllApplicationsRequest.class);
+
+    resultFlag = 0;
+    assertNotNull(client.getAllApplications(request));
+    resultFlag = 1;
+    try {
+      client.getAllApplications(request);
+      fail("should be ServiceException ");
+    } catch (YarnRemoteException e) {
+      assertEquals("YarnRemoteException test message", e.getMessage());
+    }
+    resultFlag = 2;
+    try {
+      client.getAllApplications(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+    resultFlag = 3;
+    try {
+      client.getAllApplications(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+  }
+
+  @Test
+  public void testSubmitApplication() throws YarnRemoteException {
+    SubmitApplicationRequest request = recordFactory
+        .newRecordInstance(SubmitApplicationRequest.class);
+
+    resultFlag = 0;
+    assertNotNull(client.submitApplication(request));
+    resultFlag = 1;
+    try {
+      client.submitApplication(request);
+      fail("should be ServiceException ");
+    } catch (YarnRemoteException e) {
+      assertEquals("YarnRemoteException test message", e.getMessage());
+    }
+    resultFlag = 2;
+    try {
+      client.submitApplication(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+    resultFlag = 3;
+    try {
+      client.submitApplication(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+  }
+
+  @Test
+  public void testGetNewApplication() throws YarnRemoteException {
+    GetNewApplicationRequest request = recordFactory
+        .newRecordInstance(GetNewApplicationRequest.class);
+
+    resultFlag = 0;
+    assertNotNull(client.getNewApplication(request));
+    resultFlag = 1;
+    try {
+      client.getNewApplication(request);
+      fail("should be ServiceException ");
+    } catch (YarnRemoteException e) {
+      assertEquals("YarnRemoteException test message", e.getMessage());
+    }
+    resultFlag = 2;
+    try {
+      client.getNewApplication(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+    resultFlag = 3;
+    try {
+      client.getNewApplication(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+  }
+
+  @Test
+  public void testGetClusterMetrics() throws YarnRemoteException {
+    GetClusterMetricsRequest request = recordFactory
+        .newRecordInstance(GetClusterMetricsRequest.class);
+
+    resultFlag = 0;
+    assertNotNull(client.getClusterMetrics(request));
+    resultFlag = 1;
+    try {
+      client.getClusterMetrics(request);
+      fail("should be ServiceException ");
+    } catch (YarnRemoteException e) {
+      assertEquals("YarnRemoteException test message", e.getMessage());
+    }
+    resultFlag = 2;
+    try {
+      client.getClusterMetrics(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+    resultFlag = 3;
+    try {
+      client.getClusterMetrics(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+  }
+
+  @Test
+  public void testGetApplicationReport() throws YarnRemoteException {
+    GetApplicationReportRequest request = recordFactory
+        .newRecordInstance(GetApplicationReportRequest.class);
+
+    resultFlag = 0;
+    assertNotNull(client.getApplicationReport(request));
+    resultFlag = 1;
+    try {
+      client.getApplicationReport(request);
+      fail("should be ServiceException ");
+    } catch (YarnRemoteException e) {
+      assertEquals("YarnRemoteException test message", e.getMessage());
+    }
+    resultFlag = 2;
+    try {
+      client.getApplicationReport(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+    resultFlag = 3;
+    try {
+      client.getApplicationReport(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+  }
+
+  @Test
+  public void testForceKillApplication() throws YarnRemoteException {
+    KillApplicationRequest request = recordFactory
+        .newRecordInstance(KillApplicationRequest.class);
+
+    resultFlag = 0;
+    assertNotNull(client.forceKillApplication(request));
+    resultFlag = 1;
+    try {
+      client.forceKillApplication(request);
+      fail("should be ServiceException ");
+    } catch (YarnRemoteException e) {
+      assertEquals("YarnRemoteException test message", e.getMessage());
+    }
+    resultFlag = 2;
+    try {
+      client.forceKillApplication(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
+    resultFlag = 3;
+    try {
+      client.forceKillApplication(request);
+      fail("should be UndeclaredThrowableException ");
+    } catch (UndeclaredThrowableException e) {
+
+    }
   }
 
   private static InetSocketAddress getProtocolAddress(Configuration conf)
@@ -145,99 +482,105 @@ public class TestRMAdminProtocolPBClientImpl {
     public GetNewApplicationResponseProto getNewApplication(
         RpcController controller, GetNewApplicationRequestProto request)
         throws ServiceException {
-      // TODO Auto-generated method stub
-      return null;
+      return (GetNewApplicationResponseProto) processMessage(GetNewApplicationResponseProto.getDefaultInstance());
+
     }
 
     @Override
     public GetApplicationReportResponseProto getApplicationReport(
         RpcController controller, GetApplicationReportRequestProto request)
         throws ServiceException {
-      // TODO Auto-generated method stub
-      return null;
+      return (GetApplicationReportResponseProto) processMessage(GetApplicationReportResponseProto.getDefaultInstance());
+
     }
 
     @Override
     public SubmitApplicationResponseProto submitApplication(
         RpcController controller, SubmitApplicationRequestProto request)
         throws ServiceException {
-      // TODO Auto-generated method stub
-      return null;
+      return (SubmitApplicationResponseProto) processMessage(SubmitApplicationResponseProto.getDefaultInstance());
+
     }
 
     @Override
     public KillApplicationResponseProto forceKillApplication(
         RpcController controller, KillApplicationRequestProto request)
         throws ServiceException {
-      // TODO Auto-generated method stub
-      return null;
+      return (KillApplicationResponseProto) processMessage(KillApplicationResponseProto.getDefaultInstance());
+
     }
 
     @Override
     public GetClusterMetricsResponseProto getClusterMetrics(
         RpcController controller, GetClusterMetricsRequestProto request)
         throws ServiceException {
-      // TODO Auto-generated method stub
-      return null;
+      return (GetClusterMetricsResponseProto) processMessage(GetClusterMetricsResponseProto.getDefaultInstance());
+
     }
 
     @Override
     public GetAllApplicationsResponseProto getAllApplications(
         RpcController controller, GetAllApplicationsRequestProto request)
         throws ServiceException {
-      // TODO Auto-generated method stub
-      return null;
+      return (GetAllApplicationsResponseProto) processMessage(GetAllApplicationsResponseProto.getDefaultInstance());
+
     }
 
     @Override
     public GetClusterNodesResponseProto getClusterNodes(
         RpcController controller, GetClusterNodesRequestProto request)
         throws ServiceException {
-      // TODO Auto-generated method stub
-      return null;
+      return (GetClusterNodesResponseProto) processMessage(GetClusterNodesResponseProto.getDefaultInstance());
+
     }
 
     @Override
     public GetQueueInfoResponseProto getQueueInfo(RpcController controller,
         GetQueueInfoRequestProto request) throws ServiceException {
-      // TODO Auto-generated method stub
-      return null;
+      return (GetQueueInfoResponseProto) processMessage(GetQueueInfoResponseProto.getDefaultInstance());
+
     }
 
     @Override
     public GetQueueUserAclsInfoResponseProto getQueueUserAcls(
         RpcController controller, GetQueueUserAclsInfoRequestProto request)
         throws ServiceException {
-      // TODO Auto-generated method stub
-      return null;
+      return (GetQueueUserAclsInfoResponseProto) processMessage(GetQueueUserAclsInfoResponseProto.getDefaultInstance());
+
     }
 
     @Override
     public GetDelegationTokenResponseProto getDelegationToken(
         RpcController controller, GetDelegationTokenRequestProto request)
         throws ServiceException {
-      // TODO Auto-generated method stub
-      return null;
+      return (GetDelegationTokenResponseProto) processMessage(GetDelegationTokenResponseProto.getDefaultInstance());
+
     }
 
     @Override
     public RenewDelegationTokenResponseProto renewDelegationToken(
         RpcController controller, RenewDelegationTokenRequestProto request)
         throws ServiceException {
-      // TODO Auto-generated method stub
-      return null;
+      return (RenewDelegationTokenResponseProto) processMessage(RenewDelegationTokenResponseProto.getDefaultInstance());
+
+
     }
 
     @Override
     public CancelDelegationTokenResponseProto cancelDelegationToken(
         RpcController controller, CancelDelegationTokenRequestProto request)
         throws ServiceException {
-      if (responce == 0) {
-        return CancelDelegationTokenResponseProto.getDefaultInstance();
-      } else if (responce == 1) {
-        throw new ServiceException(
-            new YarnRemoteExceptionPBImpl("YarnRemoteException test message"));
-      } else if (responce == 2) {
+        return (CancelDelegationTokenResponseProto) processMessage(CancelDelegationTokenResponseProto.getDefaultInstance());
+    
+    }
+    
+    private GeneratedMessage processMessage(GeneratedMessage result) throws ServiceException{
+      if (resultFlag == 0) {
+        return result;
+      } else if (resultFlag == 1) {
+        throw new ServiceException(new YarnRemoteExceptionPBImpl(
+            "YarnRemoteException test message"));
+      } else if (resultFlag == 2) {
         throw new ServiceException(new UndeclaredThrowableException(
             new Throwable()));
 
