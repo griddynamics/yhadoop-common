706a2dce6f7b2c9a94757e301a8760547c45ba9e
aleksgor <agorshkov@griddynamics.com>
2013-03-07 16:05:48 +0400

    format fix

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-web-proxy/src/main/java/org/apache/hadoop/yarn/server/webproxy/WebAppProxyServer.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-web-proxy/src/main/java/org/apache/hadoop/yarn/server/webproxy/WebAppProxyServer.java
@@ -1,20 +1,20 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 package org.apache.hadoop.yarn.server.webproxy;
 
@@ -32,7 +32,8 @@ import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.service.CompositeService;
 
 /**
- * ProxyServer will sit in between the end user and AppMaster web interfaces.
+ * ProxyServer will sit in between the end user and AppMaster
+ * web interfaces. 
  */
 public class WebAppProxyServer extends CompositeService {
 
@@ -42,9 +43,9 @@ public class WebAppProxyServer extends CompositeService {
   public static final int SHUTDOWN_HOOK_PRIORITY = 30;
 
   private static final Log LOG = LogFactory.getLog(WebAppProxyServer.class);
-
+  
   private WebAppProxy proxy = null;
-
+  
   public WebAppProxyServer() {
     super(WebAppProxyServer.class.getName());
   }
@@ -53,8 +54,8 @@ public class WebAppProxyServer extends CompositeService {
   public synchronized void init(Configuration conf) {
     Configuration config = new YarnConfiguration(conf);
     try {
-      doSecureLogin(conf);
-    } catch (IOException ie) {
+      doSecureLogin(conf);      
+    } catch(IOException ie) {
       throw new YarnException("Proxy Server Failed to login", ie);
     }
     proxy = new WebAppProxy();
@@ -64,25 +65,17 @@ public class WebAppProxyServer extends CompositeService {
 
   /**
    * Log in as the Kerberose principal designated for the proxy
-   * 
-   * @param conf
-   *          the configuration holding this information in it.
-   * @throws IOException
-   *           on any error.
+   * @param conf the configuration holding this information in it.
+   * @throws IOException on any error.
    */
   protected void doSecureLogin(Configuration conf) throws IOException {
     SecurityUtil.login(conf, YarnConfiguration.PROXY_KEYTAB,
         YarnConfiguration.PROXY_PRINCIPAL);
   }
 
-  /**
-   * Wait for service to finish. (Normally, it runs forever.)
-   */
-  private void join() {
-    proxy.join();
-  }
-
   public static void main(String[] args) {
+    Thread.setDefaultUncaughtExceptionHandler(new YarnUncaughtExceptionHandler());
+    StringUtils.startupShutdownMessage(WebAppProxyServer.class, args, LOG);
     try {
       startServer(args);
     } catch (Throwable t) {
@@ -95,9 +88,6 @@ public class WebAppProxyServer extends CompositeService {
    * for test this method
    */
   protected static WebAppProxyServer startServer(String[] args) throws Exception {
-    Thread
-        .setDefaultUncaughtExceptionHandler(new YarnUncaughtExceptionHandler());
-    StringUtils.startupShutdownMessage(WebAppProxyServer.class, args, LOG);
     WebAppProxyServer proxy = new WebAppProxyServer();
     ShutdownHookManager.get().addShutdownHook(
         new CompositeServiceShutdownHook(proxy), SHUTDOWN_HOOK_PRIORITY);
