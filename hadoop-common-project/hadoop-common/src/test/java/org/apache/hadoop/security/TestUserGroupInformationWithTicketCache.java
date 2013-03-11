fbdb213b2b6e7980811156caadaf74623b4e4fe1
dennisyv <dennisyv@gmail.com>
2013-03-11 18:50:02 +0400

    added license header & timeouts to TestUserGroupInformationWithTicketCache

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/security/TestUserGroupInformationWithTicketCache.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/security/TestUserGroupInformationWithTicketCache.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.apache.hadoop.security;
 
 
@@ -132,7 +148,7 @@ public class TestUserGroupInformationWithTicketCache {
     return v;
   }
 
-  @Test
+  @Test (timeout = 5000)
   public void testGetBestUGIFromTicketCache() throws Exception {
     cacheTGT(fullPrincipalName);
     
@@ -176,7 +192,7 @@ public class TestUserGroupInformationWithTicketCache {
     }
   }
 
-  @Test
+  @Test (timeout = 5000)
   public void testLoginAutoRenewalFromTicketCache() throws Exception {
     cacheTGT(fullPrincipalName);
 
@@ -275,7 +291,7 @@ public class TestUserGroupInformationWithTicketCache {
     checkFileReadable(ticketCacheFilePath);
   }
 
-  @Test
+  @Test (timeout = 5000)
   public void testCheckTGTAndReloginFromKeytab() throws Exception {
     conf = new Configuration();
     SecurityUtil.setAuthenticationMethod(AuthenticationMethod.KERBEROS, conf);
