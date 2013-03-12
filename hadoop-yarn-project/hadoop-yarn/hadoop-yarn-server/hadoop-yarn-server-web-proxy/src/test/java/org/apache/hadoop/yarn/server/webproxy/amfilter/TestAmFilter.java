cfe60c13054fe62afda900ade6a077c76f219c7d
aleksgor <agorshkov@griddynamics.com>
2013-03-12 11:27:44 +0400

    format

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-web-proxy/src/test/java/org/apache/hadoop/yarn/server/webproxy/amfilter/TestAmFilter.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-web-proxy/src/test/java/org/apache/hadoop/yarn/server/webproxy/amfilter/TestAmFilter.java
@@ -22,25 +22,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -51,14 +36,18 @@ import org.apache.hadoop.yarn.server.webproxy.WebAppProxyServlet;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+/**
+ * Test AmIpFilter. Requests to a no declared hosts should has way through proxy.
+ * Another requests can be filtered with (without) user name. 
+ * 
+ */
 public class TestAmFilter {
 
   private String proxyHost = "localhost";
   private String proxyUri = "http://bogus";
-  private AtomicBoolean invoked = new AtomicBoolean();
-  private String dofilterRequest;
+  private String doFilterRequest;
   private AmIpServletRequestWrapper servletWrapper;
-  
+
   private class TestAmIpFilter extends AmIpFilter {
 
     private Set<String> proxyAddresses = null;
@@ -100,7 +89,7 @@ public class TestAmFilter {
     }
   }
 
-  @Test
+  @Test(timeout = 5000)
   public void filterNullCookies() throws Exception {
     HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
 
@@ -108,6 +97,7 @@ public class TestAmFilter {
     Mockito.when(request.getRemoteAddr()).thenReturn(proxyHost);
 
     HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+    final AtomicBoolean invoked = new AtomicBoolean();
 
     FilterChain chain = new FilterChain() {
       @Override
@@ -127,63 +117,76 @@ public class TestAmFilter {
     assertTrue(invoked.get());
     filter.destroy();
   }
-
-  @Test
+/**
+ * Test AmIpFilter
+ */
+  @Test(timeout = 1000)
   public void testFilter() throws Exception {
     Map<String, String> params = new HashMap<String, String>();
     params.put(AmIpFilter.PROXY_HOST, proxyHost);
     params.put(AmIpFilter.PROXY_URI_BASE, proxyUri);
-    FilterConfig conf = new DummyFilterConfig(params);
+    FilterConfig config = new DummyFilterConfig(params);
 
+    // dummy filter
     FilterChain chain = new FilterChain() {
       @Override
       public void doFilter(ServletRequest servletRequest,
           ServletResponse servletResponse) throws IOException, ServletException {
-        dofilterRequest=servletRequest.getClass().getName();
+        doFilterRequest = servletRequest.getClass().getName();
         if (servletRequest instanceof AmIpServletRequestWrapper) {
-            servletWrapper = (AmIpServletRequestWrapper) servletRequest;
-          
+          servletWrapper = (AmIpServletRequestWrapper) servletRequest;
+
         }
       }
     };
     AmIpFilter testFilter = new AmIpFilter();
-    testFilter.init(conf);
-    HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
-    Mockito.when(request.getRemoteAddr()).thenReturn("reditect");
-    Mockito.when(request.getRequestURI()).thenReturn("/reditect");
+    testFilter.init(config);
 
-    MyHttpServletResponse response =new MyHttpServletResponse();
-
+    MyHttpServletResponse response = new MyHttpServletResponse();
+    // Test request should implements HttpServletRequest
     try {
       testFilter.doFilter(new MyServletRequest(), response, chain);
       fail();
     } catch (ServletException e) {
       assertEquals("This filter only works for HTTP/HTTPS", e.getMessage());
     }
+    
+    // request with HttpServletRequest
+    HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+    Mockito.when(request.getRemoteAddr()).thenReturn("redirect");
+    Mockito.when(request.getRequestURI()).thenReturn("/redirect");
     testFilter.doFilter(request, response, chain);
+    // address "redirect" is not in host list 
     assertEquals("http://bogus/reditect", response.getRedirect());
-    // without cookie 
+    // "127.0.0.1" contains in host list.   Without cookie 
     Mockito.when(request.getRemoteAddr()).thenReturn("127.0.0.1");
     testFilter.doFilter(request, response, chain);
-    
-    assertTrue( dofilterRequest.contains("javax.servlet.http.HttpServletRequest"));
+
+    assertTrue(doFilterRequest
+            .contains("javax.servlet.http.HttpServletRequest"));
     // cookie added
-    Cookie [] cookies= new Cookie[1];
-    cookies[0]= new Cookie(WebAppProxyServlet.PROXY_USER_COOKIE_NAME, "user");
-    
+    Cookie[] cookies = new Cookie[1];
+    cookies[0] = new Cookie(WebAppProxyServlet.PROXY_USER_COOKIE_NAME, "user");
+
     Mockito.when(request.getCookies()).thenReturn(cookies);
     testFilter.doFilter(request, response, chain);
 
-    assertEquals("org.apache.hadoop.yarn.server.webproxy.amfilter.AmIpServletRequestWrapper", dofilterRequest);
+    assertEquals(
+        "org.apache.hadoop.yarn.server.webproxy.amfilter.AmIpServletRequestWrapper",
+            doFilterRequest);
+    // request contains principal from cookie
     assertEquals("user", servletWrapper.getUserPrincipal().getName());
     assertEquals("user", servletWrapper.getRemoteUser());
-    assertFalse( servletWrapper.isUserInRole(""));
-    
-    
+    assertFalse(servletWrapper.isUserInRole(""));
+
   }
 
-  private class MyHttpServletResponse implements HttpServletResponse{
-    String redirectLocation="";
+  private class MyHttpServletResponse implements HttpServletResponse {
+    String redirectLocation = "";
+    public Collection<String> getHeaderNames(){
+      return null;
+    }
+
     @Override
     public String getCharacterEncoding() {
       return null;
@@ -206,22 +209,22 @@ public class TestAmFilter {
 
     @Override
     public void setCharacterEncoding(String charset) {
-      
+
     }
 
     @Override
     public void setContentLength(int len) {
-      
+
     }
 
     @Override
     public void setContentType(String type) {
-      
+
     }
 
     @Override
     public void setBufferSize(int size) {
-      
+
     }
 
     @Override
@@ -231,12 +234,12 @@ public class TestAmFilter {
 
     @Override
     public void flushBuffer() throws IOException {
-      
+
     }
 
     @Override
     public void resetBuffer() {
-      
+
     }
 
     @Override
@@ -246,12 +249,12 @@ public class TestAmFilter {
 
     @Override
     public void reset() {
-      
+
     }
 
     @Override
     public void setLocale(Locale loc) {
-      
+
     }
 
     @Override
@@ -261,7 +264,7 @@ public class TestAmFilter {
 
     @Override
     public void addCookie(Cookie cookie) {
-      
+
     }
 
     @Override
@@ -291,65 +294,109 @@ public class TestAmFilter {
 
     @Override
     public void sendError(int sc, String msg) throws IOException {
-      
+
     }
 
     @Override
     public void sendError(int sc) throws IOException {
-      
+
     }
 
-    public String getRedirect(){
+    public String getRedirect() {
       return redirectLocation;
     }
+
     @Override
     public void sendRedirect(String location) throws IOException {
-      redirectLocation=location;
+      redirectLocation = location;
     }
 
     @Override
     public void setDateHeader(String name, long date) {
-      
+
     }
 
     @Override
     public void addDateHeader(String name, long date) {
-      
+
     }
 
     @Override
     public void setHeader(String name, String value) {
-      
+
     }
 
     @Override
     public void addHeader(String name, String value) {
-      
+
     }
 
     @Override
     public void setIntHeader(String name, int value) {
-      
+
     }
 
     @Override
     public void addIntHeader(String name, int value) {
-      
+
     }
 
     @Override
     public void setStatus(int sc) {
-      
+
     }
 
     @Override
     public void setStatus(int sc, String sm) {
-      
+
     }
-    
-  } 
+
+    @Override
+    public int getStatus() {
+      return 0;
+    }
+
+    @Override
+    public String getHeader(String name) {
+      return null;
+    }
+
+    @Override
+    public Collection<String> getHeaders(String name) {
+      return null;
+    }
+
+  }
+
   private class MyServletRequest implements ServletRequest {
 
+    public AsyncContext startAsync(ServletRequest servletRequest,
+                                   ServletResponse servletResponse)
+            throws IllegalStateException{
+      return null;
+    }
+    public AsyncContext startAsync()
+            throws IllegalStateException{
+      return null;
+    }
+    public boolean isAsyncStarted(){
+      return false;
+    }
+
+    public boolean isAsyncSupported(){
+      return false;
+    }
+
+    public AsyncContext getAsyncContext(){
+      return null;
+    }
+    public DispatcherType getDispatcherType(){
+      return null;
+    }
+
+    public ServletContext getServletContext(){
+      return null;
+    }
     @Override
     public Object getAttribute(String name) {
       return null;
