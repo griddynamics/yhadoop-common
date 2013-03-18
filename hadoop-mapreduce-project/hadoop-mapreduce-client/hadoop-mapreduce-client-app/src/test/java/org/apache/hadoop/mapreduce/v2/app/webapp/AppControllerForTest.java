4dab2eac0c51f507af4491cfd41f7c7f12556ca1
aleksgor <agorshkov@griddynamics.com>
2013-03-18 14:14:53 +0400

    add tests

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/test/java/org/apache/hadoop/mapreduce/v2/app/webapp/AppControllerForTest.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/test/java/org/apache/hadoop/mapreduce/v2/app/webapp/AppControllerForTest.java
@@ -1,5 +1,8 @@
 package org.apache.hadoop.mapreduce.v2.app.webapp;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,18 +12,19 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.yarn.webapp.ResponseInfo;
 import org.apache.hadoop.yarn.webapp.View;
-import org.glassfish.grizzly.servlet.HttpServletRequestImpl;
-import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 
 public class AppControllerForTest extends AppController{
+//  private Map<String,String> properties;
+  final  private static Map<String,String>  properties = new HashMap<String, String>();
+  
+  private ResponseInfo responceInfo= new ResponseInfo();
+  private View view= new ViewForTest();
+  private Class<? extends Object>  clazz;
 
   protected AppControllerForTest(App app, Configuration conf, RequestContext ctx) {
     super(app, conf, ctx);
   }
-  private ResponseInfo responceInfo= new ResponseInfo();
-  private View view= new ViewForTest();
-  final private Map<String,String> property= new HashMap<String, String>();
-  private Class<? extends Object>  clazz;
   
   public Class<? extends Object> getClazz(){
     return clazz;
@@ -47,15 +51,19 @@ public class AppControllerForTest extends AppController{
   }
   
   public String get(String key, String defaultValue) {
-    String result =property.get(key);
+    String result =properties.get(key);
     if(result==null){
       result=defaultValue;
     }
     return result;
   }
+  public void set(String key, String value) {
+    properties.put(key, value);
+  }
+
   public HttpServletRequest request() { 
-    HttpServletRequest result= Mockito.mock(HttpServletRequest.class);
-    Mockito.when(result.getRemoteUser()).thenReturn("user");
+    HttpServletRequest result= mock(HttpServletRequest.class);
+    when(result.getRemoteUser()).thenReturn("user");
     
     
     return result; 
@@ -65,9 +73,26 @@ public class AppControllerForTest extends AppController{
   
   @Override
   public HttpServletResponse response() {
-    return super.response();
+    if(responce==null){
+     responce =mock(HttpServletResponse.class);
+    }
+    return responce;
   }
+  private HttpServletResponse responce;
   public  Map<String,String> getProperty(){
-    return property;
+    return properties;
+  }
+  
+  OutputStream data= new ByteArrayOutputStream();
+  PrintWriter writer= new PrintWriter(data);
+  public String getData(){
+    writer.flush();
+    return data.toString();
+  }
+  protected PrintWriter writer() {
+    if(writer==null){
+     writer= new PrintWriter(data);
+    }
+    return writer;
   }
 }
--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/test/java/org/apache/hadoop/mapreduce/v2/app/webapp/TestAppController.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/test/java/org/apache/hadoop/mapreduce/v2/app/webapp/TestAppController.java
@@ -26,13 +26,16 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.JobACL;
 import org.apache.hadoop.mapreduce.v2.api.records.JobId;
+import org.apache.hadoop.mapreduce.v2.api.records.TaskId;
 import org.apache.hadoop.mapreduce.v2.app.AppContext;
 import org.apache.hadoop.mapreduce.v2.app.job.Job;
+import org.apache.hadoop.mapreduce.v2.app.job.Task;
 import org.apache.hadoop.mapreduce.v2.util.MRApps;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.yarn.api.records.ApplicationId;
 import org.apache.hadoop.yarn.util.Records;
 import org.apache.hadoop.yarn.webapp.Controller.RequestContext;
+import org.apache.hadoop.yarn.webapp.MimeType;
 import org.apache.hadoop.yarn.webapp.ResponseInfo;
 import org.junit.Before;
 import org.junit.Test;
@@ -42,6 +45,8 @@ public class TestAppController {
 
   private AppControllerForTest appController;
   private RequestContext ctx;
+  private Job job;
+
 
   @Before
   public void setUp() throws IOException {
@@ -51,13 +56,18 @@ public class TestAppController {
     when(context.getApplicationName()).thenReturn("AppName");
     when(context.getUser()).thenReturn("User");
     when(context.getStartTime()).thenReturn(System.currentTimeMillis());
-    Job job=mock(Job.class);
+    job=mock(Job.class);
+    Task task= mock(Task.class);
+        
     
-    when(job.checkAccess(any(UserGroupInformation.class),any( JobACL.class))).thenReturn(true);
+    when(job.getTask(any(TaskId.class))).thenReturn(task);
+    
+   // when(job.checkAccess(any(UserGroupInformation.class),any( JobACL.class))).thenReturn(true);
     
     JobId jobID = MRApps.toJobID("job_01_01");
     when(context.getJob(jobID)).thenReturn(job);
-    
+    when(job.checkAccess(any(UserGroupInformation.class),any( JobACL.class))).thenReturn(true);
+
     
     App app = new App(context);
     Configuration conf = new Configuration();
@@ -65,6 +75,9 @@ public class TestAppController {
 
     appController = new AppControllerForTest(app, conf, ctx);
     appController.getProperty().put(AMParams.JOB_ID, "job_01_01");
+    appController.getProperty().put(AMParams.TASK_ID, "task_01_01_m01_01");
+    
+    
   }
 
   @Test
@@ -83,13 +96,14 @@ public class TestAppController {
 
   private void verifyExpectations(String message) {
     verify(ctx).setStatus(400);
-    verify(ctx).set("app.id", "application_0_0000");
-    verify(ctx).set(eq("rm.web"), anyString());
-    verify(ctx).set("title", "Bad request: " + message);
+    assertEquals("application_0_0000", appController.getProperty().get("app.id"));
+    assertNotNull( appController.getProperty().get("rm.web"));
+    assertEquals("Bad request: "+message, appController.getProperty().get("title"));
   }
 
   @Test
   public void testInfo() {
+    
     appController.info();
     Iterator<ResponseInfo.Item> iterator = appController.getResponceInfo()
         .iterator();
@@ -111,13 +125,74 @@ public class TestAppController {
   }
   @Test
   public void testGetJob() {
+    when(job.checkAccess(any(UserGroupInformation.class),any( JobACL.class))).thenReturn(false);
+    
+    appController.job();
+    verify( appController.response()).setContentType( MimeType.TEXT);
+    assertEquals("Access denied: User user does not have permission to view job job_01_01", appController.getData());
+    when(job.checkAccess(any(UserGroupInformation.class),any( JobACL.class))).thenReturn(true);
+    
+    appController.getProperty().remove(AMParams.JOB_ID);
+    appController.job();
+    assertEquals("Access denied: User user does not have permission to view job job_01_01Bad Request: Missing job ID", appController.getData());
+    
+    appController.getProperty().put(AMParams.JOB_ID, "job_01_01");
     appController.job();
     assertEquals(JobPage.class, appController.getClazz());
   }
   @Test
   public void testGetjobCounters() {
+    
+    when(job.checkAccess(any(UserGroupInformation.class),any( JobACL.class))).thenReturn(false);
+    
+    appController.jobCounters();
+    verify( appController.response()).setContentType( MimeType.TEXT);
+    assertEquals("Access denied: User user does not have permission to view job job_01_01", appController.getData());
+    when(job.checkAccess(any(UserGroupInformation.class),any( JobACL.class))).thenReturn(true);
+    
+    appController.getProperty().remove(AMParams.JOB_ID);
+    appController.jobCounters();
+    assertEquals("Access denied: User user does not have permission to view job job_01_01Bad Request: Missing job ID", appController.getData());
+    
+    appController.getProperty().put(AMParams.JOB_ID, "job_01_01");
     appController.jobCounters();
     assertEquals(CountersPage.class, appController.getClazz());
+  }
+  
+  
+  @Test
+  public void testGetTaskCounters() {
+    
+    when(job.checkAccess(any(UserGroupInformation.class),any( JobACL.class))).thenReturn(false);
+    
+    appController.taskCounters();
+    verify( appController.response()).setContentType( MimeType.TEXT);
+    assertEquals("Access denied: User user does not have permission to view job job_01_01", appController.getData());
+    
+    when(job.checkAccess(any(UserGroupInformation.class),any( JobACL.class))).thenReturn(true);
+    
+    appController.getProperty().remove(AMParams.TASK_ID);
+    appController.taskCounters();
+    assertEquals("Access denied: User user does not have permission to view job job_01_01missing task ID", appController.getData());
+    
+    appController.getProperty().put(AMParams.TASK_ID, "task_01_01_m01_01");
+    appController.taskCounters();
+    assertEquals(CountersPage.class, appController.getClazz());
+  }
+  
+  @Test
+  public void testGetSingleJobCounter() throws IOException {
+    appController.singleJobCounter();
+    assertEquals(SingleCounterPage.class, appController.getClazz());
+  }
+  
+  @Test
+  public void testGetSingleTaskCounter() throws IOException {
+    appController.singleTaskCounter();
+    assertEquals(SingleCounterPage.class, appController.getClazz());
+    assertNotNull(appController.getProperty().get(AppController.COUNTER_GROUP));
+    assertNotNull(appController.getProperty().get(AppController.COUNTER_NAME));
+
   }
   
 }
