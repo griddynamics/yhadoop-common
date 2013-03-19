4643fb5608737efdf73cf3008077bbbb4c85c170
aleksgor <agorshkov@griddynamics.com>
2013-03-19 09:24:06 +0400

    add tests

---
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/test/java/org/apache/hadoop/yarn/webapp/view/BlockForTest.java
@@ -0,0 +1,14 @@
+package org.apache.hadoop.yarn.webapp.view;
+
+import java.io.PrintWriter;
+
+public class BlockForTest  extends HtmlBlock.Block {
+
+  public BlockForTest(HtmlBlock htmlBlock, PrintWriter out, int level,
+      boolean wasInline) {
+    htmlBlock.super(out, level, wasInline);
+  }
+
+  
+  
+}
