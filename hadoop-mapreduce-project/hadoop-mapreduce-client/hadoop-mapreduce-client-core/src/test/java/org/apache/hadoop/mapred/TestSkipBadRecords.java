28ec10453f68c743586ec75124fb4c4f5a875127
aleksgor <agorshkov@griddynamics.com>
2013-03-21 11:44:18 +0400

    add timeout

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/test/java/org/apache/hadoop/mapred/TestSkipBadRecords.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/test/java/org/apache/hadoop/mapred/TestSkipBadRecords.java
@@ -28,7 +28,7 @@ import static org.junit.Assert.*;
  * 
  */
 public class TestSkipBadRecords {
-  @Test
+  @Test (timeout=5000)
   public void testSkipBadRecords() {
     // test default values
     Configuration conf = new Configuration();
