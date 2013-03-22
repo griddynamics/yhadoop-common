2d810044ca1c974229be5146387838077cf125e9
aleksgor <agorshkov@griddynamics.com>
2013-03-22 09:35:05 +0400

    update timeout

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/src/test/java/org/apache/hadoop/mapreduce/v2/TestMRJobsWithHistoryService.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/src/test/java/org/apache/hadoop/mapreduce/v2/TestMRJobsWithHistoryService.java
@@ -112,7 +112,7 @@ public class TestMRJobsWithHistoryService {
     }
   }
 
-  @Test (timeout = 30000)
+  @Test (timeout = 300000)
   public void testJobHistoryData() throws IOException, InterruptedException,
       AvroRemoteException, ClassNotFoundException {
     if (!(new File(MiniMRYarnCluster.APPJAR)).exists()) {
